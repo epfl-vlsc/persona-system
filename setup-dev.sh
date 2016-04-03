8492a51b5d58839573226c8091ddb94adba9be41
Sam Whitlock <sam.whitlock@epfl.ch>
2016-04-03 19:24:13 +0200

    scaling threads better

--- setup-dev.sh
+++ setup-dev.sh
@@ -4,8 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 build_virtualenv_dir="${DIR}/_python_build"
 dev_dir="${DIR}/python_dev"
-max_build_threads=6
-
+max_build_threads=$(bc <<< "scale=0; ($(nproc) * 0.6) / 1" )
 
 prep_virtualenv() {
     if [[ $OSTYPE == darwin* ]]; then
@@ -21,6 +20,7 @@ prep_virtualenv() {
 }
 
 build_tensorflow() {
+    echo "Building using $max_build_threads threads"
     bazel build -j $max_build_threads -c opt //tensorflow/tools/pip_package:build_pip_package
 }
 
