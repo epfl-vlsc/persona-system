e493e3f3621d4f5bcc4026dcd699de4e21726da0
Sam Whitlock <sam.whitlock@epfl.ch>
2016-04-04 09:22:39 +0200

    adding protection against having bazel overwhelm local system

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 
+echo "This builds the Python package and installs it on the local system"
+
 rm /tmp/tensorflow_pkg/*.whl
 bazel build -c opt //tensorflow/tools/pip_package:build_pip_package
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-
-bazel build -c opt --verbose_failures //tensorflow/tools/pip_package:build_pip_package
+max_build_threads=$(bc <<< "scale=0; ($(nproc) * 0.6) / 1" )
+bazel build -j $max_build_threads -c opt --verbose_failures //tensorflow/tools/pip_package:build_pip_package
 
