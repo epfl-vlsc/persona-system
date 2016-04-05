4b70877362478892fc515b478937bbe588b4d0a5
Sam Whitlock <sam.whitlock@epfl.ch>
2016-04-05 12:09:38 +0200

    adding dbg or opt build option for compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,31 @@
 #!/usr/bin/env bash
 
+# unofficial "bash strict mode"
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+# Commented out 'u' because the `activate` script has some unassignment issues
+# set -u
+set -eo pipefail
+IFS=$'\n\t'
+
+build_type="opt"
+
+process_args() {
+case "$#" in
+    0)
+        ;;
+    1)
+        build_type="$1"
+        ;;
+    *)
+        echo "Script only accepts 0 or 1 argument!"
+        exit 2
+        ;;
+esac
+}
+
+process_args "$@"
+
+echo "Building configuration $build_type"
 max_build_threads=$(bc <<< "scale=0; ($(nproc) * 0.6) / 1" )
 bazel build -j $max_build_threads -c opt --verbose_failures //tensorflow/tools/pip_package:build_pip_package
 
