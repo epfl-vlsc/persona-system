50faed32ae54aadcec5fb187e55707624dd0d764
Sam Whitlock <sam.whitlock@epfl.ch>
2016-04-20 11:15:53 +0200

    more build fixes

--- tensorflow/core/lib/core/object_pool.h
+++ tensorflow/core/lib/core/object_pool.h
@@ -9,6 +9,7 @@
 #include <deque>
 #include <utility>
 #include "tensorflow/core/platform/macros.h"
+#include "tensorflow/core/platform/mutex.h"
 
 namespace tensorflow {
 
@@ -16,37 +17,38 @@ template <typename T>
 class ObjectPool {
 public:
   explicit ObjectPool(size_t max_elements, std::function<T*()> object_constructor) :
-  max_elements_(max_elements), object_constructor_(object_constructor) {}
+            max_elements_(max_elements), object_constructor_(object_constructor) {}
 
   std::pair<std::shared_ptr<T>, std::function<void()>> GetObject(bool block = true) noexcept
-    {
-      mutex_lock l(mu_);
-      if (!ready_objects_.empty()) {
-        auto ptr = ready_objects_.pop_front();
-        return make_pair(ptr, [this, ptr]() {
-            ReleaseObject(ptr);
-          });
-      } else if (all_objects_.size() < max_elements_) {
-        auto ptr = shared_ptr<T>(object_constructor_());
-        // We can at least check if the constructor returns NULL
-        // We can't verify if the memory is valid or anything
-        if (ptr != nullptr) {
-          all_objects_.push_back(ptr);
-          return make_pair(ptr, [this, ptr]() {
-              ReleaseObject(ptr);
-            });
-        }
-      } else if (block) {
-        cv_.wait(l, [this]() {
-            !ready_objects_.empty();
-          });
-        auto ptr = ready_objects_.pop_front();
+  {
+    using namespace std;
+    mutex_lock l(mu_);
+    if (!ready_objects_.empty()) {
+      auto ptr = ready_objects_.pop_front();
+      return make_pair(ptr, [this, ptr]() {
+          ReleaseObject(ptr);
+        });
+    } else if (all_objects_.size() < max_elements_) {
+      auto ptr = shared_ptr<T>(object_constructor_());
+      // We can at least check if the constructor returns NULL
+      // We can't verify if the memory is valid or anything
+      if (ptr != nullptr) {
+        all_objects_.push_back(ptr);
         return make_pair(ptr, [this, ptr]() {
             ReleaseObject(ptr);
           });
       }
-      return make_pair(nullptr, []() {});
+    } else if (block) {
+      cv_.wait(l, [this]() {
+          !ready_objects_.empty();
+        });
+      auto ptr = ready_objects_.pop_front();
+      return make_pair(ptr, [this, ptr]() {
+          ReleaseObject(ptr);
+        });
     }
+    return make_pair(nullptr, []() {});
+  }
 
 private:
   TF_DISALLOW_COPY_AND_ASSIGN(ObjectPool);
