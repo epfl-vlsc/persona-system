78dc7cdf914f89ad81baa48c4be6309526c1f2ac
Sam Whitlock <sam.whitlock@epfl.ch>
2016-04-20 10:49:17 +0200

    Moving object pooling to header only
    Would do with inverse .hxx file, but Bazel complains
    so this is easier.

--- tensorflow/core/lib/core/object_pool.h
+++ tensorflow/core/lib/core/object_pool.h
@@ -8,18 +8,57 @@
 #include <vector>
 #include <deque>
 #include <utility>
+#include "tensorflow/core/platform/macros.h"
 
 namespace tensorflow {
 
 template <typename T>
 class ObjectPool {
- public:
-  explicit ObjectPool(size_t max_elements, std::function<T*()> object_constructor);
+public:
+  explicit ObjectPool(size_t max_elements, std::function<T*()> object_constructor) :
+  max_elements_(max_elements), object_constructor_(object_constructor) {}
 
-  std::pair<std::shared_ptr<T>, std::function<void()>> GetObject(bool block = true) noexcept;
+  std::pair<std::shared_ptr<T>, std::function<void()>> GetObject(bool block = true) noexcept
+    {
+      mutex_lock l(mu_);
+      if (!ready_objects_.empty()) {
+        auto ptr = ready_objects_.pop_front();
+        return make_pair(ptr, [this, ptr]() {
+            ReleaseObject(ptr);
+          });
+      } else if (all_objects_.size() < max_elements_) {
+        auto ptr = shared_ptr<T>(object_constructor_());
+        // We can at least check if the constructor returns NULL
+        // We can't verify if the memory is valid or anything
+        if (ptr != nullptr) {
+          all_objects_.push_back(ptr);
+          return make_pair(ptr, [this, ptr]() {
+              ReleaseObject(ptr);
+            });
+        }
+      } else if (block) {
+        cv_.wait(l, [this]() {
+            !ready_objects_.empty();
+          });
+        auto ptr = ready_objects_.pop_front();
+        return make_pair(ptr, [this, ptr]() {
+            ReleaseObject(ptr);
+          });
+      }
+      return make_pair(nullptr, []() {});
+    }
 
- private:
-  void ReleaseObject(std::shared_ptr<T> object) noexcept;
+private:
+  TF_DISALLOW_COPY_AND_ASSIGN(ObjectPool);
+
+  void ReleaseObject(std::shared_ptr<T> object) noexcept
+  {
+    mutex_lock l(mu_);
+    // TODO for now, just assume that object is in all_objects_
+    // because the only way to access this is via the function
+    ready_objects_.push_back(object);
+    cv_.notify_one();
+  }
 
   mutable std::mutex mu_;
   mutable std::condition_variable cv_;
