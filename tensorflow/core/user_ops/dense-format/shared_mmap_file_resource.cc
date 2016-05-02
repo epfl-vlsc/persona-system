1a787c90a7568889deff66948176b33d83d19ddf
Sam Whitlock <sam.whitlock@epfl.ch>
2016-05-02 08:46:52 +0200

    moving mmap file to normal structure

---
+++ tensorflow/core/user_ops/dense-format/shared_mmap_file_resource.cc
@@ -0,0 +1,22 @@
+/*
+  An op to read from a queue of filename strings, and enqueue multiple shared resources corresponding to each file.
+ */
+
+#include "shared_mmap_file_resource.h"
+
+namespace tensorflow {
+
+    MemoryMappedFile::MemoryMappedFile(ResourceHandle &mapped_file) :
+      file_(mapped_file) {}
+
+    ReadOnlyMemoryRegion *
+    MemoryMappedFile::GetMappedRegion() {
+      return file_.get();
+    }
+
+    string MemoryMappedFile::DebugString()
+    {
+      return "a Memory Mapped File";
+    }
+
+} // namespace tensorflow {
--- tensorflow/core/user_ops/dense-format/shared_mmap_file_resource.h
+++ tensorflow/core/user_ops/dense-format/shared_mmap_file_resource.h
@@ -2,34 +2,32 @@
   An op to read from a queue of filename strings, and enqueue multiple shared resources corresponding to each file.
  */
 
+#ifndef TENSORFLOW_CORE_USEROPS_DENSE_ALIGN_MMAP_H_
+#define TENSORFLOW_CORE_USEROPS_DENSE_ALIGN_MMAP_H_
+
 #include <memory>
 #include "tensorflow/core/platform/file_system.h"
 #include "tensorflow/core/framework/resource_mgr.h"
 
 namespace tensorflow {
-  using namespace std;
 
   /*
     Just a convenience class to track a read-only memory region throughout the execution context.
    */
   class MemoryMappedFile : public ResourceBase {
   public:
-    typedef shared_ptr<ReadOnlyMemoryRegion> ResourceHandle;
+    typedef std::shared_ptr<ReadOnlyMemoryRegion> ResourceHandle;
 
-    explicit MemoryMappedFile(ResourceHandle &mapped_file) :
-      file_(mapped_file) {}
+    explicit MemoryMappedFile(ResourceHandle &mapped_file);
 
-    ReadOnlyMemoryRegion *
-    GetMappedRegion() {
-      return file_.get();
-    }
+    ReadOnlyMemoryRegion* GetMappedRegion();
 
-    string DebugString() override
-    {
-      return "a Memory Mapped File";
-    }
+    string DebugString() override;
 
   private:
        ResourceHandle file_;
   };
+
 } // namespace tensorflow {
+
+#endif
