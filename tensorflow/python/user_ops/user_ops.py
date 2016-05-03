f9d92e63ae03e0d4c56b6552be80dc3b8bd841c3
Sam Whitlock <sam.whitlock@epfl.ch>
2016-05-03 13:48:44 +0200

    fixing python userops interface

--- tensorflow/python/user_ops/user_ops.py
+++ tensorflow/python/user_ops/user_ops.py
@@ -46,30 +46,52 @@ def FASTQDecoder(value):
 
 ops.NoGradient("FASTQDecoder")
 
-def DenseReader(batch_size):
-  return gen_user_ops.dense_reader(batch_size=batch_size)
+def DenseReader(file_handle, batch_size):
+  return gen_user_ops.dense_reader(file_handle=file_handle, batch_size=batch_size)
 ops.NoGradient("DenseReader")
 @ops.RegisterShape("DenseReader")
 def _DenseReaderShape(op):
   import ipdb; ipdb.set_trace()
-  return [tensor.unknown_shape()]
-# TODO this is not the right shape
+  # just force the input to be a vector (will raise an exception if incorrect)
+  input_shape = op.inputs[0].get_shape().with_rank(1)
+  batch_size = op.get_attr("batch_size")
+  return [tensor_shape.TensorShape([batch_size]), common_shapes.scalar_shape]
+
+def DenseAggregator(bases, bases_count, qualities, qualities_count, metadata, metadata_count):
+  return gen_user_ops.dense_aggregator(bases=bases,
+                                       bases_count=bases_count,
+                                       qualities=qualities,
+                                       qualities_count=qualities_count,
+                                       metadata=metadata,
+                                       metadata_count=metadata_count)
 
 @ops.RegisterShape("DenseAggregator")
 def _DenseAggregatorShape(op): # pylint: disable=invalid-name
   # TODO this is such a hack
-  return [tensor_shape.unknown_shape()]
+  bases = op.inputs[0]
+  base_count = op.inputs[1]
+  qualities = op.inputs[2]
+  qualities_count = op.inputs[3]
+  metadata = op.inputs[4]
+  metadata_count = op.inputs[5]
 
-def DenseAggregator(bases, qualities, metadata):
-  return gen_user_ops.dense_aggregator(bases=bases,
-                                       qualities=qualities,
-                                       metadata=metadata)
+  base_count_force = base_count.get_shape().with_rank(0)
+  qualities_count_force = qualities_count.get_shape().with_rank(0)
+  metadata_count_force = metadata_count.get_shape().with_rank(0)
+  bases_force = bases.get_shape().with_rank(1)
+  qualities_force = qualities.get_shape().with_rank(1)
+  metadata_force = metadata.get_shape().with_rank(1)
+
+  return [tensor_shape.TensorShape([3, bases_force[0]])]
 ops.NoGradient("DenseAggregator")
 
 def FileMMap(queue):
   return gen_user_ops.file_mmap(queue_handle=queue)
+
+@ops.RegisterShape("FileMMap")
+def _FileMMapSHape(op):
+  return [tensor_shape.TensorShape([2])]
 ops.NoGradient("FileMMap")
-ops.RegisterShape("FileMMap")(common_shapes.scalar_shape)
 
 class SAMWriter(io_ops.WriterBase):
 
