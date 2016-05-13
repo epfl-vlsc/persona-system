fffc809ed4dbf1aabbf8940487f19613ff0b1d47
Sam Whitlock <sam.whitlock@epfl.ch>
2016-05-13 11:20:36 +0200

    fixing new read decode format

--- tensorflow/core/user_ops/dna-align/snap_read_decode.cc
+++ tensorflow/core/user_ops/dna-align/snap_read_decode.cc
@@ -18,33 +18,37 @@ SnapReadDecode::SnapReadDecode(const Tensor* read_tensor) {
 
 const char* SnapReadDecode::bases(int batch_index) {
   auto reads = read_tensor_->matrix<string>();
-  return reads(batch_index, kBases).c_str();
+  return reads(kBases, batch_index).c_str();
 }
 const char* SnapReadDecode::qualities(int batch_index) {
   auto reads = read_tensor_->matrix<string>();
-  return reads(batch_index, kQualities).c_str();
+  return reads(kQualities, batch_index).c_str();
 }
 const char* SnapReadDecode::metadata(int batch_index) {
   auto reads = read_tensor_->matrix<string>();
-  return reads(batch_index, kMetadata).c_str();
+  return reads(kMetadata, batch_index).c_str();
 }
 int SnapReadDecode::metadata_len(int batch_index) {
   auto reads = read_tensor_->matrix<string>();
-  return reads(batch_index, kMetadata).length();
+  return reads(kMetadata, batch_index).length();
 }
 int SnapReadDecode::bases_len(int batch_index) {
   auto reads = read_tensor_->matrix<string>();
-  return reads(batch_index, kBases).length();
+  return reads(kBases, batch_index).length();
 }
 
 // mutable
 MutableSnapReadDecode::MutableSnapReadDecode(Tensor* read_tensor) {
   if (read_tensor->dims() != 2) {
-    LOG(INFO) << "Error: trying to create read decoder with tensor"
+    LOG(WARNING) << "Error: trying to create read decoder with tensor"
       << " not of dim 2";
   }
+  if (read_tensor->dim_size(0) != 3) {
+    LOG(WARNING) << "Error: need read tensor of dim-0 size of 3, but got "
+              << read_tensor->dim_size(0);
+  }
   if (read_tensor->dtype() != DT_STRING) {
-    LOG(INFO) << "Error: trying to create read decoder with non "
+    LOG(WARNING) << "Error: trying to create read decoder with non "
       << "string type tensor.";
   }
   read_tensor_ = read_tensor;
@@ -52,35 +56,35 @@ MutableSnapReadDecode::MutableSnapReadDecode(Tensor* read_tensor) {
 
 const char* MutableSnapReadDecode::bases(int batch_index) {
   auto reads = read_tensor_->matrix<string>();
-  return reads(batch_index, kBases).c_str();
+  return reads(kBases, batch_index).c_str();
 }
 void MutableSnapReadDecode::set_bases(int batch_index, const string& bases) {
   auto reads = read_tensor_->matrix<string>();
-  reads(batch_index, kBases) = bases;
+  reads(kBases, batch_index) = bases;
 }
 const char* MutableSnapReadDecode::qualities(int batch_index) {
   auto reads = read_tensor_->matrix<string>();
-  return reads(batch_index, kQualities).c_str();
+  return reads(kQualities, batch_index).c_str();
 }
 void MutableSnapReadDecode::set_qualities(int batch_index, const string& quals) {
   auto reads = read_tensor_->matrix<string>();
-  reads(batch_index, kQualities) = quals;
+  reads(kQualities, batch_index) = quals;
 }
 const char* MutableSnapReadDecode::metadata(int batch_index) {
   auto reads = read_tensor_->matrix<string>();
-  return reads(batch_index, kMetadata).c_str();
+  return reads(kMetadata, batch_index).c_str();
 }
 void MutableSnapReadDecode::set_metadata(int batch_index, const string& meta) {
   auto reads = read_tensor_->matrix<string>();
-  reads(batch_index, kMetadata) = meta;
+  reads(kMetadata, batch_index) = meta;
 }
 int MutableSnapReadDecode::metadata_len(int batch_index) {
   auto reads = read_tensor_->matrix<string>();
-  return reads(batch_index, kMetadata).length();
+  return reads(kMetadata, batch_index).length();
 }
 int MutableSnapReadDecode::bases_len(int batch_index) {
   auto reads = read_tensor_->matrix<string>();
-  return reads(batch_index, kBases).length();
+  return reads(kBases, batch_index).length();
 }
 
 }
