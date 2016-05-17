db9accef37384854473ffa30851138869bd3bf1d
Sam Whitlock <sam.whitlock@epfl.ch>
2016-05-17 22:00:43 +0200

    adding a scope timer to write out to a file

---
+++ tensorflow/core/user_ops/dense-format/scope_timer.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <chrono>
+#include <sstream>
+#include "tensorflow/core/platform/file_system.h"
+#include "tensorflow/core/platform/logging.h"
+
+namespace tensorflow {
+  using namespace std;
+
+  class ScopeWriter {
+  public:
+  ScopeWriter(WritableFile *trace_file) : trace_file_(trace_file),
+      start_tick_(chrono::high_resolution_clock::now()),
+      start_time_(chrono::system_clock::now()) {}
+
+    ~ScopeWriter() {
+      using namespace chrono;
+
+      auto tick_diff = high_resolution_clock::now() - start_tick_;
+      auto start_time_us = duration_cast<microseconds>(start_time_.time_since_epoch());
+      ostringstream a;
+      a << start_time_us.count() << "," << tick_diff.count() << "\n";
+      auto s = trace_file_->Append(a.str());
+      if (!s.ok()) {
+        LOG(WARNING) << "Unable to append to trace file";
+      }
+    }
+  private:
+    WritableFile *trace_file_;
+
+    chrono::high_resolution_clock::time_point start_tick_;
+    chrono::system_clock::time_point start_time_;
+  };
+} // namespace tensorflow
