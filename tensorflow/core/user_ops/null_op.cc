092bc48c8da55ce1fbf2fc67d3bf0eb05f02814e
Sam Whitlock <sam.whitlock@epfl.ch>
2016-05-19 17:45:41 +0200

    committing null op

---
+++ tensorflow/core/user_ops/null_op.cc
@@ -0,0 +1,30 @@
+#include "tensorflow/core/framework/op_kernel.h"
+#include "tensorflow/core/framework/op.h"
+
+namespace tensorflow {
+  using namespace std;
+
+  REGISTER_OP("SinkOp")
+  .Attr("T: {float, int32, string, float32}")
+  .Input("data: T")
+  .Doc(R"doc(
+Consumes the input and produces nothing
+)doc");
+
+  class SinkOp : public OpKernel {
+  public:
+    SinkOp(OpKernelConstruction* context) : OpKernel(context) {}
+
+    void Compute(OpKernelContext* ctx) override {
+      // TODO do I have to even do anything with it?
+      const Tensor *input_tensor;
+      OP_REQUIRES_OK(ctx, ctx->input("data", &input_tensor));
+    }
+  };
+
+#define REGISTER_TYPE(TYPE) \
+  REGISTER_KERNEL_BUILDER(Name("SinkOp").Device(DEVICE_CPU).TypeConstraint<TYPE>("T"), \
+                          SinkOp)
+
+REGISTER_TYPE(string);
+} // namespace tensorflow {
