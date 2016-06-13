bfdbcd87502108ea1e04b45f56f5ccc66adc706d
Sam Whitlock <sam.whitlock@epfl.ch>
2016-06-13 13:32:06 +0200

    adding better memcpy support for char bufs

--- tensorflow/core/user_ops/dense-format/decompress.cc
+++ tensorflow/core/user_ops/dense-format/decompress.cc
@@ -1,15 +1,17 @@
 #include "decompress.h"
 
+#include <cstring>
 #include <boost/iostreams/filtering_stream.hpp>
 
 namespace tensorflow {
 
   //auto decompressBZIP2 = decompressSegment<boost::iostreams::bzip2_decompressor>;
+  using namespace std;
 
 template <typename T>
 Status decompressSegment(const char* segment,
-                         const std::size_t segment_size,
-                         std::vector<char> &output)
+                         const size_t segment_size,
+                         vector<char> &output)
 {
   namespace bsio = boost::iostreams;
   bsio::filtering_ostream os;
@@ -18,7 +20,7 @@ Status decompressSegment(const char* segment,
   // We can't be sure exactly how much data it will produce.
 
   os.push(T());
-  os.push(std::back_inserter(output));
+  os.push(back_inserter(output));
   os.write(segment, segment_size);
 
   // Not sure if these need to be called, or if the destructor gets them
@@ -29,20 +31,26 @@ Status decompressSegment(const char* segment,
 
 template
 Status decompressSegment<boost::iostreams::bzip2_decompressor>(const char* segment,
-                                                                const std::size_t segment_size,
-                                                                std::vector<char> &output);
+                                                                const size_t segment_size,
+                                                                vector<char> &output);
 
 template
 Status decompressSegment<boost::iostreams::gzip_decompressor>(const char* segment,
-                                                                const std::size_t segment_size,
-                                                                std::vector<char> &output);
+                                                                const size_t segment_size,
+                                                                vector<char> &output);
 
 Status copySegment(const char* segment,
-                   const std::size_t segment_size,
-                   std::vector<char> &output)
+                   const size_t segment_size,
+                   vector<char> &output)
 {
   output.reserve(segment_size);
-  output.insert(output.end(), segment, segment+segment_size);
+  if (output.capacity() < segment_size) {
+    // just use normal insert and not the optimized memcpy
+    output.insert(output.end(), segment, segment+segment_size);
+  } else {
+    memcpy(&output[0], segment, segment_size);
+    output.resize(segment_size);
+  }
   return Status::OK();
 }
 
