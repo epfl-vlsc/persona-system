0fe51d059dff0527ea05f86f17e46546e115ac1a
Sam Whitlock <sam.whitlock@epfl.ch>
2016-07-06 13:59:26 +0200

    adding header-writing capability to column writer

--- tensorflow/core/user_ops/dense-format/column_writer_op.cc
+++ tensorflow/core/user_ops/dense-format/column_writer_op.cc
@@ -1,8 +1,11 @@
 #include "tensorflow/core/user_ops/object-pool/resource_container.h"
+#include "tensorflow/core/lib/core/errors.h"
 #include "data.h"
 #include <cstdio>
+#include <cstring>
 #include <vector>
 #include "compression.h"
+#include "format.h"
 
 namespace tensorflow {
 
@@ -10,6 +13,11 @@ namespace tensorflow {
   .Attr("compress: bool = true")
   .Input("column_handle: string")
   .Input("file_path: string")
+  // TODO these can be collapsed into a vec(3) if that would help performance
+  .Input("first_ordinal: int64")
+  .Input("num_records: int64")
+  .Input("record_type: int32")
+  .Input("record_id: string")
   .SetIsStateful()
   .Doc(R"doc(
 Writes out a column (just a character buffer) to the location specified by the input.
@@ -18,6 +26,7 @@ This writes out to local disk only
 )doc");
 
   using namespace std;
+  using namespace errors;
 
   class ColumnWriterOp : public OpKernel {
   public:
@@ -42,6 +51,8 @@ This writes out to local disk only
       OP_REQUIRES(ctx, file_out != NULL,
                   Internal("Unable to open file at path:", filepath));
 
+      OP_REQUIRES_OK(ctx, WriteHeader(ctx, file_out));
+
       int fwrite_ret;
       auto s = Status::OK();
       // TODO in the future, override with a separate op to avoid an if statement
@@ -65,7 +76,38 @@ This writes out to local disk only
     }
 
   private:
+
+    Status WriteHeader(OpKernelContext *ctx, FILE *file_out) {
+      // TODO get all the incoming parameters for the chunk and write them out
+      const Tensor *tensor;
+      uint64_t tmp64;
+      TF_RETURN_IF_ERROR(ctx->input("first_ordinal", &tensor));
+      tmp64 = static_cast<uint64_t>(tensor->scalar<int64>()());
+      header_.first_ordinal = tmp64;
+
+      TF_RETURN_IF_ERROR(ctx->input("num_records", &tensor));
+      tmp64 = static_cast<uint64_t>(tensor->scalar<int64>()());
+      header_.last_ordinal = header_.first_ordinal + tmp64;
+
+      TF_RETURN_IF_ERROR(ctx->input("record_type", &tensor));
+      auto record_type = static_cast<uint8_t>(tensor->scalar<int32>()());
+      header_.record_type = record_type;
+
+      TF_RETURN_IF_ERROR(ctx->input("record_id", &tensor));
+      auto record_id = tensor->scalar<string>()();
+      strncpy(header_.string_id, record_id.c_str(), sizeof(header_.string_id));
+      header_.string_id[sizeof(header_.string_id)-1] = '\0'; // to make sure it's null-terminated, per strncpy manpage
+
+      int fwrite_ret = fwrite(&header_, sizeof(header_), 1, file_out);
+      if (fwrite_ret != 1) {
+        // TODO get errno out of the file
+        return Internal("frwrite(header_) failed");
+      }
+      return Status::OK();
+    }
+
     bool compress_;
     vector<char> buf_; // used to compress into
+    format::FileHeader header_;
   };
 } //  namespace tensorflow {
