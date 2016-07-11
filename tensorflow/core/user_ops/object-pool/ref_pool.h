bd22eebe3c11ab5a50403e04edbeb85d690b2698
Sam Whitlock <sam.whitlock@epfl.ch>
2016-07-11 16:49:47 +0200

    adding get unbound method for pools

--- tensorflow/core/user_ops/object-pool/ref_pool.h
+++ tensorflow/core/user_ops/object-pool/ref_pool.h
@@ -40,6 +40,37 @@ namespace tensorflow {
     // For getting / releasing resources
     // locks are grabbed here
     Status GetResource(ResourceContainer<T> **rct) {
+      if (unbound_) {
+        return GetUnbound(rct);
+      } else {
+        return GetBound(rct);
+      }
+    }
+
+    string DebugString() override {
+      static const string s = "a reference pool";
+      return s;
+    }
+
+  private:
+
+    inline Status GetUnbound(ResourceContainer<T> **rct) {
+      mutex_lock l(objects_mu_);
+      if (run_) {
+        if (objects_.empty()) {
+          TF_RETURN_IF_ERROR(creator_(this));
+        }
+        // at this point, we are guaranteed that the object has been added to the pool
+        auto s = objects_.front();
+        *rct = s;
+        objects_.pop_front();
+        return Status::OK();
+      } else {
+        return errors::Aborted("Reference Pool has been stopped");
+      }
+    }
+
+    inline Status GetBound(ResourceContainer<T> **rct) {
       mutex_lock l(objects_mu_);
       // while instead of for in case multiple are woken up
       while (objects_.empty() && run_) {
@@ -58,13 +89,6 @@ namespace tensorflow {
         return errors::Aborted("Reference Pool has been stopped");
       }
     }
-
-    string DebugString() override {
-      static const string s = "a reference pool";
-      return s;
-    }
-
-  private:
 
     std::vector<ResourceContainer<T>*> all_objects_;
     std::deque<ResourceContainer<T>*> objects_;
