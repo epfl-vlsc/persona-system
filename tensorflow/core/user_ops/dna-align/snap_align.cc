1ec79d0881bdc2a58fe27b904af3d2552bc345e8
Stuart Byma <stuart.byma@epfl.ch>
2016-07-14 15:53:09 +0200

    modified snap align to remove time consuming allocations

--- tensorflow/core/user_ops/dna-align/snap_align.cc
+++ tensorflow/core/user_ops/dna-align/snap_align.cc
@@ -56,7 +56,7 @@ class SnapAlignOp : public OpKernel {
         }
       }
 
-      boost::timer::auto_cpu_timer t;
+      //boost::timer::auto_cpu_timer t;
 
       const Tensor* reads;
       OP_REQUIRES_OK(ctx, ctx->input("read", &reads));
@@ -64,21 +64,30 @@ class SnapAlignOp : public OpKernel {
       //LOG(INFO) << "reads shape is: " << reads->shape().DebugString();
       SnapReadDecode read_batch(reads);
       size_t num_reads = read_batch.size();
+      size_t reads_encountered = num_reads;
+      
+      Tensor* out = nullptr;
+      OP_REQUIRES_OK(ctx, ctx->allocate_output(0, 
+            SnapResultsDecode::get_results_shape(num_reads, 
+              num_secondary_alignments_), &out));
 
-      vector<Read*> input_reads;
-      input_reads.reserve(num_reads);
+      MutableSnapResultsDecode results(out);
+      
+      bool first_is_primary;
+      AlignerOptions* options = options_resource_->value();
 
       for (size_t i = 0; i < num_reads; i++) {
+
         if (read_batch.bases_len(i) == 0) {
+          reads_encountered = i;
           LOG(INFO) << "string was empty, is this a partial batch?";
-          continue;
+          break;
         }
         /*LOG(INFO) << "Read from decoder:";
         LOG(INFO) << "Meta: " << read_batch.metadata(i) <<  "\nBases: " << read_batch.bases(i)
           << "\nQual: " << read_batch.qualities(i) << "\nbase len: " << read_batch.bases_len(i);*/
       
-        Read* snap_read = new Read();
-        snap_read->init(
+        snap_read_.init(
             read_batch.metadata(i),  // id
             read_batch.metadata_len(i), // id len
             read_batch.bases(i),  // data (bases)
@@ -86,65 +95,70 @@ class SnapAlignOp : public OpKernel {
             read_batch.bases_len(i)  // data len
             );
 
-        input_reads.push_back(snap_read);
-      }
+        if (!passesReadFilter(&snap_read_, options_resource_->value())) {
+          results.set_first_is_primary(i, true);
+          results.set_num_results(i, 1); 
+          results.set_result_type(i, 1, (int64)AlignmentResult::NotFound); // cast from enum
+          results.set_genome_location(i, 1, InvalidGenomeLocation);
+          results.set_score(i, 1, 0);
+          results.set_mapq(i, 1, 0);
+          results.set_direction(i, 1, 0);
+          continue;
+        }
 
+        SingleAlignmentResult primaryResult;
+        int num_secondary_alignments = 0;
+        int num_secondary_results;
+        base_aligner_->AlignRead(
+          &snap_read_,
+          &primaryResult,
+          options->maxSecondaryAlignmentAdditionalEditDistance,
+          num_secondary_alignments * sizeof(SingleAlignmentResult),
+          &num_secondary_results,
+          num_secondary_alignments,
+          nullptr //secondaryResults
+        );
 
-      size_t num_actual_reads = input_reads.size();
-      vector<SingleAlignmentResult> alignment_results;
+        if (options->passFilter(&snap_read_, primaryResult.status, false, false)) {
+          first_is_primary = true;
+        }
+        else {
+          first_is_primary = false;
+        }
 
-      Tensor* out = nullptr;
-      OP_REQUIRES_OK(ctx, ctx->allocate_output(0, 
-            SnapResultsDecode::get_results_shape(num_reads, 
-              num_secondary_alignments_), &out));
-
-      MutableSnapResultsDecode results(out);
-
-      OP_REQUIRES_OK(ctx, ctx->set_output("reads_out", *reads));
-      //forward_ref_input_to_ref_output(0, 1);
-
-      //LOG(INFO) << "num actual reads is " << num_actual_reads;
-      bool first_is_primary;
-      for (size_t i = 0; i < num_actual_reads; i++) {
-        Status status = snap_wrapper::alignSingle(base_aligner_, options_resource_->value(), input_reads[i],
-            &alignment_results, num_secondary_alignments_, first_is_primary);
-
-        //LOG(INFO) << "Result for read " << input_reads[i]->getData();
         results.set_first_is_primary(i, first_is_primary);
-        results.set_num_results(i, alignment_results.size()); 
-        for (int j = 0; j < alignment_results.size(); j++) {
-          SingleAlignmentResult result = alignment_results[j];
-          /*LOG(INFO) << "Type/status: " << result.status << "Location: " << GenomeLocationAsInt64(result.location)
-            << " score: " << result.score << " mapq: " << result.mapq << " direction: " << result.direction;*/
-          results.set_result_type(i, j, (int64)result.status); // cast from enum
-          results.set_genome_location(i, j, GenomeLocationAsInt64(result.location));
-          results.set_score(i, j, result.score);
-          results.set_mapq(i, j, result.mapq);
-          results.set_direction(i, j, result.direction);
-        }
-        alignment_results.clear();
+        results.set_num_results(i, 1); 
+        results.set_result_type(i, 0, (int64)primaryResult.status); // cast from enum
+        results.set_genome_location(i, 0, GenomeLocationAsInt64(primaryResult.location));
+        results.set_score(i, 0, primaryResult.score);
+        results.set_mapq(i, 0, primaryResult.mapq);
+        results.set_direction(i, 0, primaryResult.direction);
 
-        // TODO(solal): Smart pointers would probably make this unnecessary, but I'm not knowledgeable enough in C++ to do that
-        delete input_reads[i];
-
-        if (!status.ok()) {
-          LOG(INFO) << "SnapAlignOp: alignSingle failed!!";
-        }
       }
 
-      //LOG(INFO) << "actual: " << num_actual_reads << " total: " << num_reads;
-      for (size_t i = num_actual_reads; i < num_reads; i++) {
+
+      for (size_t i = reads_encountered; i < num_reads; i++) {
         // for uneven batches, set the num of results to 0
         LOG(INFO) << "setting 0 for uneven batch";
         results.set_num_results(i, 0);
       }
+      
+      OP_REQUIRES_OK(ctx, ctx->set_output("reads_out", *reads));
+
+      //LOG(INFO) << "actual: " << num_actual_reads << " total: " << num_reads;
     }
 
   private:
+
+    bool passesReadFilter(Read* read, AlignerOptions* options) {
+      return read->getDataLength() >= options->minReadLength && read->countOfNs() <= options->maxDist;
+    }
+
     BaseAligner* base_aligner_ = nullptr;
     int num_secondary_alignments_ = 0;
     GenomeIndexResource* index_resource_ = nullptr;
     AlignerOptionsResource* options_resource_ = nullptr;
+    Read snap_read_;
 
 };
 
