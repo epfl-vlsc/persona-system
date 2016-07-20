583f8d2dcdb2d9b9f3f1418c58d7e72921b9de61
Sam Whitlock <sam.whitlock@epfl.ch>
2016-07-20 11:12:47 +0200

    adding gzip op untested, but easy to fix any errors

--- tensorflow/python/user_ops/user_ops.py
+++ tensorflow/python/user_ops/user_ops.py
@@ -307,3 +307,19 @@ ops.NoGradient(_fcp_str)
 @ops.RegisterShape(_fcp_str)
 def _FASTQCreatorPoolOpShape(op):
     return [tensor_shape.vector(2)]
+
+_gz_str = "GZIPDecomp"
+
+def GZIPDecompressor(buffer_pool, data_handle, name=None):
+  return gen_user_ops.gzip_decomp(buffer_pool=buffer_pool,
+                                  data_handle=data_handle,
+                                  name=name)
+
+ops.NoGradient(_gz_str)
+@ops.RegisterShape(_gz_str)
+def _GZIPDecompShape(op):
+  pool_shape = op.inputs[0].get_shape()
+  data_shape = op.inputs[1].get_shape()
+  _assert_vec(pool_shape, 2)
+  _assert_vec(data_shape, 2)
+  return [data_shape]
