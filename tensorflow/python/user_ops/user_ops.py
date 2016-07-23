6bdc059fbc0bd9d5921eb5086e0a1f403280b697
Sam Whitlock <sam.whitlock@epfl.ch>
2016-07-23 12:55:34 +0200

    some python3 fixes and adding default outputdir for column writer

--- tensorflow/python/user_ops/user_ops.py
+++ tensorflow/python/user_ops/user_ops.py
@@ -230,10 +230,12 @@ def _BufferPoolShape(op):
 
 _cw_str = "ColumnWriter"
 allowed_type_values = set(["base", "qual", "meta", "results"])
-def ColumnWriter(column_handle, file_path, first_ordinal, num_records, record_id, record_type, compress=False, name=None):
+def ColumnWriter(column_handle, file_path, first_ordinal, num_records, record_id, record_type, compress=False, output_dir="", name=None):
     if record_type not in allowed_type_values:
         raise Exception("record_type ({given}) for ColumnWriter must be one of the following values: {expected}".format(
           given=record_type, expected=allowed_type_values))
+    if output_dir != "" and output_dir[-1] != "/":
+      output_dir += "/"
     return gen_user_ops.column_writer(
       column_handle=column_handle,
       file_path=file_path,
@@ -242,6 +244,7 @@ def ColumnWriter(column_handle, file_path, first_ordinal, num_records, record_id
       num_records=num_records,
       compress=compress,
       record_id=record_id,
+      output_dir=output_dir,
       name=name
     )
 
@@ -250,7 +253,7 @@ ops.NoGradient(_cw_str)
 def _ColumnWriterShape(op):
   column_handle_shape = op.inputs[0].get_shape()
   _assert_vec(column_handle_shape, 2)
-  for i in xrange(1,4):
+  for i in range(1,4):
     _assert_scalar(op.inputs[i].get_shape())
   return []
 
@@ -294,7 +297,7 @@ def FASTQCreator(data_handle, pool_handle, name=None):
 ops.NoGradient(_fc_str)
 @ops.RegisterShape(_fc_str)
 def _FASTQCreatorOPShape(op):
-    for i in xrange(2):
+    for i in range(2):
         a = op.inputs[i].get_shape()
         _assert_vec(a, 2)
     return [tensor_shape.vector(2)]
