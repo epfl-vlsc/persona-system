b38797fc25215dac6e4dd0a489fef5825168d0c2
Sam Whitlock <sam.whitlock@epfl.ch>
2016-07-25 20:35:47 +0200

    adding forgotten tracepoint

--- tensorflow/core/user_ops/dense-format/file_mmap_op.cc
+++ tensorflow/core/user_ops/dense-format/file_mmap_op.cc
@@ -124,8 +124,10 @@ bundle_name: [{this map op's name}] + upstream_name
       ResourceContainer<MemoryMappedFile> *mmf;
       OP_REQUIRES_OK(ctx, ref_pool->GetResource(&mmf));
 
+      auto start = clock();
       ReadOnlyMemoryRegion *rmr;
       OP_REQUIRES_OK(ctx, ctx->env()->NewReadOnlyMemoryRegionFromFile(filename, &rmr));
+      tracepoint(bioflow, file_mmap, clock() - start, filename.c_str());
       mmf->get()->own(rmr);
 
       Tensor *output_tensor;
