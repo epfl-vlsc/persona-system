945ea0e50f344d6ac86ae0b3ad092a814443df1d
Stuart Byma <stuart.byma@epfl.ch>
2016-07-28 11:58:09 +0200

    fix snap align dense to do correct pre processing

--- tensorflow/core/user_ops/dna-align/snap_align_dense.cc
+++ tensorflow/core/user_ops/dna-align/snap_align_dense.cc
@@ -98,7 +98,7 @@ class SnapAlignDenseOp : public OpKernel {
       {
         auto start = clock();
         ReadResourceReleaser r(*reads);
-        bool first_is_primary;
+        bool first_is_primary = true; // we only ever generate one result
         cigarString_.clear();
         const char *bases, *qualities;
         std::size_t bases_len, qualities_len;
@@ -111,6 +111,17 @@ class SnapAlignDenseOp : public OpKernel {
 
           snap_read_.init(nullptr, 0, bases, qualities, bases_len);
           snap_read_.clip(options_->clipping);
+          if (!options_->passFilter(&snap_read_, AlignmentResult::NotFound, true, false)) {
+            LOG(INFO) << "FILTERING READ";
+          } else {
+            primaryResult.status = AlignmentResult::NotFound;
+            primaryResult.location = InvalidGenomeLocation;
+            primaryResult.mapq = 0;
+            primaryResult.direction = FORWARD;
+            cigarString_.clear();
+            result_builder_.AppendAlignmentResult(primaryResult, cigarString_, alignment_result_buffer);
+            continue;
+          }
 
           base_aligner_->AlignRead(
             &snap_read_,
@@ -123,6 +134,8 @@ class SnapAlignDenseOp : public OpKernel {
           );
 
           flag_ = 0;
+
+          // we may need to do post process options->passfilter here?
 
           // compute the CIGAR strings and flags
           // input_reads[i] holds the current snap_read
