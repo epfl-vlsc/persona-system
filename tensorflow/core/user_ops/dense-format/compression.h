044e5a312cb654e76b2dc63041dc21df2bdf418e
Sam Whitlock <sam.whitlock@epfl.ch>
2016-07-29 15:08:36 +0200

    some buffer_list adaptation, plus partial work on gzip appending

--- tensorflow/core/user_ops/dense-format/compression.h
+++ tensorflow/core/user_ops/dense-format/compression.h
@@ -3,15 +3,37 @@
 #include "tensorflow/core/lib/core/errors.h"
 
 #include <vector>
+#include <zlib.h>
 
 namespace tensorflow {
 
-Status decompressGZIP(const char* segment,
+  Status decompressGZIP(const char* segment,
+                        const std::size_t segment_size,
+                        std::vector<char> &output);
+
+  Status compressGZIP(const char* segment,
                       const std::size_t segment_size,
                       std::vector<char> &output);
 
- Status compressGZIP(const char* segment,
-                     const std::size_t segment_size,
-                     std::vector<char> &output);
+  class AppendingGZIPCompressor
+  {
+  public:
+    AppendingGZIPCompressor(std::vector<char> &output);
+
+    ~AppendingGZIPCompressor();
+
+    // reinitializes the stream
+    Status init();
+
+    Status appendGZIP(const char* segment,
+                      const std::size_t segment_size);
+
+    // closes the stream
+    Status finish(); // somehow flush
+  private:
+    z_stream stream_ = {0};
+    bool done_ = false;
+    std::vector<char> &output_;
+  };
 
 } // namespace tensorflow
