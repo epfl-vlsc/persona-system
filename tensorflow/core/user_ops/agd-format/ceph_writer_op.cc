4635f41d06786d6490e9462d44924bd904a54038
Stuart Byma <stuart.byma@epfl.ch>
2016-09-23 12:19:44 +0200

    fixed ceph writer to output key

--- tensorflow/core/user_ops/agd-format/ceph_writer_op.cc
+++ tensorflow/core/user_ops/agd-format/ceph_writer_op.cc
@@ -35,7 +35,7 @@ namespace tensorflow {
   .Input("file_name: string")
   .Input("first_ordinal: int64")
   .Input("num_records: int32")
-  .Output("num_records_out: int32")
+  .Output("key_out: string")
   .Doc(R"doc(
 Writes data in column_handle to object file_name in specified Ceph cluster.
 
@@ -202,8 +202,8 @@ compress: whether or not to compress the column
       tracepoint(bioflow, chunk_write, filepath.c_str(), start);
 
       Tensor *num_recs;
-      OP_REQUIRES_OK(ctx, ctx->allocate_output("num_records_out", TensorShape({}), &num_recs));
-      num_recs->scalar<int32>()() = num_records;
+      OP_REQUIRES_OK(ctx, ctx->allocate_output("key_out", TensorShape({}), &num_recs));
+      num_recs->scalar<string>()() = file_path;
     }
 
   private:
