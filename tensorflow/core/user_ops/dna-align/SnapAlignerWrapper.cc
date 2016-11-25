6d460b66a653b8ca80d20e79d1e34bb3da1175d3
Sam Whitlock <sam.whitlock@epfl.ch>
2016-11-25 13:03:41 +0100

    removing more old code

--- tensorflow/core/user_ops/dna-align/SnapAlignerWrapper.cc
+++ tensorflow/core/user_ops/dna-align/SnapAlignerWrapper.cc
@@ -182,28 +182,6 @@ namespace snap_wrapper {
     return Status::OK();
   }
 
-  BaseAligner* createAligner(GenomeIndex* index, AlignerOptions* options) {
-    return new BaseAligner(
-        index,
-        options->maxHits,
-        options->maxDist,
-        MAX_READ_LENGTH,
-        options->numSeedsFromCommandLine,
-        options->seedCoverage,
-        options->minWeightToCheck,
-        options->extraSearchDepth,
-        false, false, false, // stuff that would decrease performance without impacting quality
-        options->maxSecondaryAlignmentsPerContig,
-        nullptr, nullptr, // Uncached Landau-Vishkin
-        nullptr, // No need for stats
-        nullptr // No special allocator
-        );
-  }
-
-  bool passesReadFilter(Read* read, AlignerOptions* options) {
-    return read->getDataLength() >= options->minReadLength && read->countOfNs() <= options->maxDist;
-  }
-
   bool computeCigarFlags(
     // input
     Read *read,
@@ -401,86 +379,5 @@ namespace snap_wrapper {
     return tensorflow::Status::OK();
 	} // adjustResults
 
-  tensorflow::Status writeRead(const ReaderContext& context, 
-      Read *read, SingleAlignmentResult *results, int nResults,
-      bool firstIsPrimary, char* buffer, uint64 buffer_size,
-      uint64* buffer_used, const FileFormat* format,
-      LandauVishkinWithCigar& lvc, const Genome* genome) {
-
-    //LOG(INFO) << "SnapWrapper writing read! buffer_size=" << 
-    //  buffer_size << ", nResults=" << nResults;
-    uint64 used = 0;
-
-    for (int i = 0; i < nResults; i++) {
-      if (results[i].status == NotFound) {
-        results[i].location = InvalidGenomeLocation;
-      }
-    }
-
-    GenomeLocation finalLocation;
-
-    // this is adapted from SNAP, some things are a little arcane
-    for (int whichResult = 0; whichResult < nResults; whichResult++) {
-      int addFrontClipping = 0;
-      read->setAdditionalFrontClipping(0);
-      int cumulativeAddFrontClipping = 0;
-      finalLocation = results[whichResult].location;
-
-      unsigned nAdjustments = 0;
-      size_t used_local;
-      while (!format->writeRead(context, &lvc, buffer + used, buffer_size - used,
-            &used_local, read->getIdLength(), read, 
-            results[whichResult].status,results[whichResult].mapq, 
-            finalLocation, results[whichResult].direction, 
-            (whichResult > 0) || !firstIsPrimary, &addFrontClipping)) {
-
-        nAdjustments++;
-
-        if (0 == addFrontClipping) {
-          // *this* is how SNAP notifies you there wasn't enough 
-          // space in the buffer. Much intuitive, very logic, wow. 
-          // caller will call again with fresh buffer
-          return tensorflow::errors::ResourceExhausted("buffer too full in SNAP writeRead"); 
-        }
-
-        // redo if read modified (e.g. to add soft clipping, or move alignment for a leading I.
-        const Genome::Contig *originalContig = results[whichResult].status == NotFound ? NULL
-          : genome->getContigAtLocation(results[whichResult].location);
-        const Genome::Contig *newContig = results[whichResult].status == NotFound ? NULL
-          : genome->getContigAtLocation(results[whichResult].location + addFrontClipping);
-        if (newContig == NULL || newContig != originalContig || finalLocation
-            + addFrontClipping > originalContig->beginningLocation 
-            + originalContig->length - genome->getChromosomePadding() ||
-            nAdjustments > read->getDataLength()) {
-          //
-          // Altering this would push us over a contig boundary, or we're stuck in a loop.  Just give up on the read.
-          //
-          results[whichResult].status = NotFound;
-          results[whichResult].location = InvalidGenomeLocation;
-          finalLocation = InvalidGenomeLocation;
-        } else {
-          cumulativeAddFrontClipping += addFrontClipping;
-          if (addFrontClipping > 0) {
-            read->setAdditionalFrontClipping(cumulativeAddFrontClipping);
-          }
-          finalLocation = results[whichResult].location + cumulativeAddFrontClipping;
-        }
-      } // while formatting doesn't work
-      //LOG(INFO) << "Formatting was successful, used " << used_local << " bytes";
-      used += used_local;
-      if (used > buffer_size) {
-        // shouldn't happen, but of course is error
-        return tensorflow::errors::Internal("Buffer overflow in Snapwrapper read writer?");
-      }
-
-      if (used > 0xffffffff) {
-        return tensorflow::errors::Internal("Snap Read writer:writeReads: used too big");
-      }
-    } // for each result.
-
-    read->setAdditionalFrontClipping(0);
-    *buffer_used = used;
-    return tensorflow::Status::OK();
-  }
 }
 
--- tensorflow/core/user_ops/dna-align/SnapAlignerWrapper.h
+++ tensorflow/core/user_ops/dna-align/SnapAlignerWrapper.h
@@ -41,8 +41,6 @@ namespace snap_wrapper {
       std::string cigar;
     };
 
-    BaseAligner* createAligner(GenomeIndex* index, AlignerOptions* options);
-
     // Uses SNAP code to compute the 'cigar' and 'flags' fields from the SAM format
     bool computeCigarFlags(
         // inputs
@@ -73,13 +71,4 @@ namespace snap_wrapper {
         //outputs
         std::string &cigarString,
         int &flags);
-
-
-    // uses slightly modified SNAP code to write results in 
-    // `format` format to the provided buffer
-    Status writeRead(const ReaderContext& context, 
-        Read *read, SingleAlignmentResult *results, int nResults,
-        bool firstIsPrimary, char* buffer, uint64 buffer_size,
-        uint64* buffer_used, const FileFormat* format, 
-        LandauVishkinWithCigar& lvc, const Genome* genome);
 }
