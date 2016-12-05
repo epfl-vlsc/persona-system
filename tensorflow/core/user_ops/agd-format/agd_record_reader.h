4f6c20f45770670c2d00d4cbd81a1c53c318a8ae
Sam Whitlock <sam.whitlock@epfl.ch>
2016-12-05 10:52:15 +0100

    separating record reader into hdr and src files

--- tensorflow/core/user_ops/agd-format/agd_record_reader.h
+++ tensorflow/core/user_ops/agd-format/agd_record_reader.h
@@ -9,53 +9,26 @@ namespace tensorflow {
 
   using namespace errors;
 
+  /*
+   * A class that provides a "view" over the data in the resource container.
+   * Does not take ownership of the underlying data
+   */
   class AGDRecordReader {
-    public:
-      AGDRecordReader(ResourceContainer<Data>* resource, int num_records) {
-        num_records_ = num_records;
-        auto idx_offset = num_records * sizeof(RelativeIndex);
-        base_resource_ = resource;
-        base_data_ = base_resource_->get()->data();
-        index_ = reinterpret_cast<const RelativeIndex*>(base_data_);
-        cur_data_ = data_ = base_data_ + idx_offset;
-        cur_record_ = 0;
-      }
+  public:
+    AGDRecordReader(ResourceContainer<Data>* resource, size_t num_records);
 
-      void Reset() {
-        cur_data_ = data_;
-        cur_record_ = 0;
-      }
+    void Reset();
 
-      Status GetNextRecord(const char** data, uint32* size) {
-        if (cur_record_ < num_records_) {
-          *size = (uint32) index_[cur_record_];
-          *data = cur_data_;
-          cur_data_ += index_[cur_record_];
-          cur_record_++;
-          return Status::OK();
-        } else
-          return ResourceExhausted("agd record container exhausted");
-      }
+    Status GetNextRecord(const char** data, size_t* size);
 
-      Status GetRecordAt(const char** data, uint32* size, int index) {
-        if (index < num_records) {
-          uint32 offset = 0;
-          *size = index_[0];
-          for (int i = 0; i < index; i++) {
-            offset += index_[i];
-            *size = index_[i];
-          }
-          *data = data_ + offset;
-        } else
-          return OutOfRange("agd record random access out of range");
-      }
+    Status GetRecordAt(size_t index, const char** data, size_t* size);
 
-    private:
-      const format::RelativeIndex *index_, cur_index_;
-      const char *base_data_, data_, cur_data_;
-      int num_records_;
-      int cur_record_;
-  }
+  private:
+    const format::RelativeIndex *index_;
+    const char *data_, *cur_data_;
+    const size_t num_records_;
+    size_t cur_record_ = 0;
+  };
 
 }
 
