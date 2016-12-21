5313a8f967f2a2b664419ed964f1883033da99aa
Sam Whitlock <sam.whitlock@epfl.ch>
2016-12-21 12:01:38 +0100

    changing meta -> metadata everywhere

--- tensorflow/core/user_ops/agd-format/parallel_column_writer_op.cc
+++ tensorflow/core/user_ops/agd-format/parallel_column_writer_op.cc
@@ -24,7 +24,7 @@ namespace tensorflow {
   REGISTER_OP(op_name.c_str())
   .Attr("compress: bool")
   .Attr("record_id: string")
-  .Attr("record_type: {'base', 'qual', 'meta', 'results'}")
+  .Attr("record_type: {'base', 'qual', 'metadata', 'results'}")
   .Attr("output_dir: string = ''")
   .Input("column_handle: string")
   .Input("file_path: string")
@@ -63,7 +63,7 @@ Thus we always need 3 of these for the full conversion pipeline
         t = RecordType::BASES;
       } else if (s.compare("qual") == 0) {
         t = RecordType::QUALITIES;
-      } else if (s.compare("meta") == 0) {
+      } else if (s.compare("metadata") == 0) {
         t = RecordType::COMMENTS;
       } else { // no need to check. we're saved by string enum types if TF
         t = RecordType::ALIGNMENT;
