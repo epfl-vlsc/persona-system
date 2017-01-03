a74a5ee706bd8b67f3919c692364d428c9fdb4fe
Stuart Byma <stuart.byma@epfl.ch>
2017-01-03 15:27:19 +0100

    adding remote record reader

--- tensorflow/core/user_ops/agd-format/agd_record_reader.cc
+++ tensorflow/core/user_ops/agd-format/agd_record_reader.cc
@@ -66,4 +66,141 @@ namespace tensorflow {
     }
     return Status::OK();
   }
+
+
+  AGDRemoteRecordReader::AGDRemoteRecordReader(string filename, size_t num_records, 
+        char* buffer, size_t buffer_size, librados::IoCtx* io_ctx) :
+    io_ctx_(io_ctx), base_buf_(buffer), base_size_(buffer_size), num_records_(num_records),
+    filename_(filename) {
+
+    current_offset_ = sizeof(format::FileHeader);
+    buf_0_.data = base_buf_ + num_records_;
+    auto size = (buffer_size - num_records_) / 2;
+    buf_1_.data = buf_0_.data + size;
+    buf_0_.size = base_size_ - (base_buf_ + base_size_ - buf_1_.data) - num_records_;
+    buf_1_.size = base_size_ - buf_0_.size - num_records_;
+    buf_0_.recs = 0;
+    buf_1_.recs = 0;
+  }
+
+  Status AGDRemoteRecordReader::ReadData(char* dest, size_t size) {
+    librados::bufferlist read_buf;
+    read_buf.push_back(ceph::buffer::create_static(size, dest));
+    librados::AioCompletion *read_completion = librados::Rados::aio_create_completion();
+    int ret = io_ctx_->aio_read(filename_, read_completion, &read_buf, size, current_offset_);
+    if (ret < 0) {
+      return errors::Internal("Couldn't start read object in remote record reader");
+    }
+    read_completion->wait_for_complete();
+    ret = read_completion->get_return_value();
+    if (ret < 0) {
+      LOG(INFO) << "Couldn't read object! error " << ret;
+      return errors::Internal("Couldn't read ceph object in remote record reader");
+    }
+    read_buf.clear();
+    read_completion->release();
+
+    current_offset_ += size;
+    return Status::OK();
+  }
+
+  Status AGDRemoteRecordReader::Initialize() {
+    if (init)
+      return Status::OK();
+
+    Status s = ReadData(base_buf_, num_records_);
+    if (!s.ok())
+      return s;
+    index_ = reinterpret_cast<const RelativeIndex*>(base_buf_);
+    size_t total = 0;
+    size_t i;
+    for (i = 0; i < num_records_; i++) {
+      if (total + index_[i] < buf_0_.size)
+        total += index_[i];
+      else
+        break;
+    }
+    cur_record_prefetched_ = i;
+    s = ReadData(buf_0_.data, total);
+    if (!s.ok())
+      return s;
+    buf_0_.recs = i;
+    buf_0_.cur_data = buf_0_.data;
+    active_buf_ = &buf_0_;
+    return Status::OK();
+  }
+
+  AGDRemoteRecordReader::RecordBuffer* AGDRemoteRecordReader::OtherBuffer() {
+    if (active_buf_ == &buf_0_)
+      return &buf_1_;
+    else
+      return &buf_0_;
+  }
+
+  Status AGDRemoteRecordReader::GetNextRecord(const char** data, size_t* size) {
+    auto s = PeekNextRecord(data, size);
+    if (s.ok()) {
+      cur_record_++;
+      if (active_buf_->recs > 1)
+        active_buf_->recs--;
+      else if (active_buf_->recs == 1) {
+        // we need to switch buffers, but make sure the other is filled first
+        auto other_buf = OtherBuffer();
+        if (other_buf->recs == 0) {
+          mutex_lock l(mu_);
+          // may need to make recs volatile?
+          ready_cv_.wait(l, [other_buf]() {
+              return other_buf->recs == 0;
+            });
+        }
+        active_buf_->recs = 0;
+        active_buf_ = other_buf;
+        return s;
+      }
+    }
+    return s;
+  }
+  
+  Status AGDRemoteRecordReader::PeekNextRecord(const char** data, size_t* size) {
+    if (!init)
+      return Internal("Tried to use uninitialized remote record reader");
+
+    if (cur_record_ < num_records_) {
+      *size = (size_t) index_[cur_record_];
+      //*data = cur_data_;
+      if (active_buf_->recs == 0)
+        return Internal("The active buffer of a remote record reader has 0 records!");
+
+      *data = active_buf_->cur_data;
+    } else {
+      return ResourceExhausted("agd record container exhausted");
+    }
+    return Status::OK();
+  }
+
+  Status AGDRemoteRecordReader::PrefetchRecords() {
+    auto other_buf = OtherBuffer();
+    if (other_buf->recs == 0 && cur_record_prefetched_ < num_records_) { 
+      // fill that shit
+      size_t total = 0;
+      size_t i;
+      for (i = cur_record_prefetched_; i < num_records_; i++) {
+        if (total + index_[i] < other_buf->size) {
+          total += index_[i]; 
+        } else
+          break;
+      }
+      auto recs = i - cur_record_prefetched_;
+      cur_record_prefetched_ = i;
+      Status s = ReadData(other_buf->data, total);
+      if (!s.ok())
+        return s;
+      other_buf->cur_data = other_buf->data;
+      other_buf->recs = recs;
+    } else if (cur_record_prefetched_ == num_records_)
+      return ResourceExhausted("Remote data in ceph is fully prefetched");
+
+    return Status::OK();
+  }
+
 } // namespace tensorflow {
--- tensorflow/core/user_ops/agd-format/agd_record_reader.h
+++ tensorflow/core/user_ops/agd-format/agd_record_reader.h
@@ -5,6 +5,8 @@
 #include "data.h"
 #include "format.h"
 #include <vector>
+#include <rados/librados.hpp>
+#include <rados/buffer.h>
 
 namespace tensorflow {
 
@@ -34,6 +36,61 @@ namespace tensorflow {
     std::vector<size_t> absolute_index_;
 
     void InitializeIndex();
+  };
+
+  /* Provides a "view" over remote data in a ceph object store.
+   * does not own buffers or io_ctx. Simply uses them to get and
+   * buffer data. Only supports sequential access of records.
+   *
+   * `buffer` should be large enough to hold a reasonable number 
+   * of records. It is split in 3 and used to hold the index
+   * and a double buffer of records read from Ceph.
+   */
+  class AGDRemoteRecordReader {
+  public:
+    AGDRemoteRecordReader(string filename, size_t num_records, 
+        char* buffer, size_t buffer_size, librados::IoCtx* io_ctx);
+
+    void Reset();
+
+    Status GetNextRecord(const char** data, size_t* size);
+    Status PeekNextRecord(const char** data, size_t* size);
+
+    Status PrefetchRecords();
+    Status Initialize();
+
+  private:
+  
+    Status ReadData(char* dest, size_t size);
+
+    struct RecordBuffer {
+      char* data;
+      const char* cur_data;
+      size_t size;
+      size_t recs;
+    };
+
+    RecordBuffer* OtherBuffer();
+
+    const format::RelativeIndex *index_;
+    const char *cur_data_;
+    size_t num_records_;
+    size_t cur_record_ = 0;
+    size_t cur_record_prefetched_ = 0;
+    librados::IoCtx* io_ctx_ = nullptr;
+    char* base_buf_;
+
+    // two buffers, a basic double buffering scheme
+    RecordBuffer buf_0_;
+    RecordBuffer buf_1_;
+    size_t base_size_ = 0;
+    RecordBuffer* active_buf_ = nullptr;
+    size_t current_offset_ = 0; // current offset into ceph object
+    string filename_; // ceph object full path within `io_ctx_`
+
+    mutable mutex mu_;
+    mutable std::condition_variable ready_cv_;
+    bool init = false;
   };
 
 }
