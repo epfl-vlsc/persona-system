2d450a6e60d41fc6542cecf1a1cc8278d61791a4
Stuart Byma <stuart.byma@epfl.ch>
2017-02-04 15:09:19 +0100

    multithread bam output working

--- tensorflow/core/user_ops/bam/agd_output_bam_op.cc
+++ tensorflow/core/user_ops/bam/agd_output_bam_op.cc
@@ -1,13 +1,19 @@
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/user_ops/object-pool/basic_container.h"
 #include "tensorflow/core/framework/shape_inference.h"
+#include "tensorflow/core/lib/core/threadpool.h"
 #include <sstream>
+#include <tuple>
+#include <thread>
+#include <vector>
 #include "tensorflow/core/user_ops/object-pool/resource_container.h"
 #include "tensorflow/core/user_ops/object-pool/ref_pool.h"
 #include "tensorflow/core/user_ops/agd-format/agd_record_reader.h"
 #include "tensorflow/core/user_ops/agd-format/format.h"
 #include "tensorflow/core/user_ops/dna-align/snap/SNAPLib/Bam.h"
 #include "zlib.h"
+#include "work_queue.h"
+#include "priority_work_queue.h"
 
 namespace tensorflow {
   using namespace std;
@@ -29,6 +35,7 @@ namespace tensorflow {
     .Attr("ref_seq_sizes: list(int)")
     .Attr("read_group: string")
     .Attr("sort_order: {'unknown', 'unsorted', 'queryname', 'coordinate'}")
+    .Attr("num_threads: int >= 2")
     .Input("results_handle: string")
     .Input("bases_handle: string")
     .Input("qualities_handle: string")
@@ -53,6 +60,8 @@ namespace tensorflow {
     read_group: read group tag @RG
     *handles: the records to append to the BAM file
     num_records: the number of records held in *handles
+    num_threads: number of threads for compression >= 2 because one is
+    used to coordinate writing to disk.
     )doc");
 
   class AgdOutputBamOp : public OpKernel {
@@ -66,12 +75,13 @@ namespace tensorflow {
         OP_REQUIRES_OK(ctx, ctx->GetAttr("ref_sequences", &ref_seqs_));
         OP_REQUIRES_OK(ctx, ctx->GetAttr("ref_seq_sizes", &ref_sizes_));
         OP_REQUIRES_OK(ctx, ctx->GetAttr("sort_order", &sort_order));
+        OP_REQUIRES_OK(ctx, ctx->GetAttr("num_threads", &num_threads_));
 
         OP_REQUIRES(ctx, ref_seqs_.size() == ref_sizes_.size(), 
             Internal("ref seqs was not same size as ref seq sizes lists"));
         stringstream header_ss;
-        header_ss << "@HD VN:1.4 SO:";
-        header_ss << sort_order;
+        header_ss << "@HD\tVN:1.4\tSO:";
+        header_ss << sort_order << endl;
         /*for (int i = 0; i < ref_seqs.size(); i++) {
           header_ss << "@SQ SN:" << ref_seqs[i] << " LN:" << to_string(ref_seq_sizes[i]) << endl;
         }*/
@@ -84,41 +94,66 @@ namespace tensorflow {
         }
         // open the file, we dont write yet
         bam_fp_ = fopen(path.c_str(), "w");
-        string header = header_ss.str();
+        header_ = header_ss.str();
 
-        scratch_.reset(new char[scratch_size_]); // 64K max size of BAM block
-        scratch_compress_.reset(new char[scratch_size_]); // 64K max size of BAM block
-        scratch_pos_ = 0;
+        //LOG(INFO) << "num_threads is " << num_threads_;
+        buffer_queue_.reset(new WorkQueue<BufferRef>(num_threads_*2));
+        compress_queue_.reset(new WorkQueue<CompressItem>(num_threads_*2));
+        write_queue_.reset(new PriorityWorkQueue<WriteItem>(num_threads_*2));
 
-        BAMHeader* bamHeader = (BAMHeader*) scratch_.get();
-        bamHeader->magic = BAMHeader::BAM_MAGIC;
-        size_t samHeaderSize = header.length();
-        memcpy(bamHeader->text(), header.c_str(), samHeaderSize);
-        bamHeader->l_text = (int)samHeaderSize;
-        scratch_pos_ = BAMHeader::size((int)samHeaderSize);
-		
-        bamHeader->n_ref() = ref_seqs_.size();
-		    BAMHeaderRefSeq* refseq = bamHeader->firstRefSeq();
-        for (int i = 0; i < ref_seqs_.size(); i++) {
-          int len = ref_seqs_[i].length() + 1;
-          scratch_pos_ += BAMHeaderRefSeq::size(len);
-          refseq->l_name = len;
-          memcpy(refseq->name(), ref_seqs_[i].c_str(), len);
-          refseq->l_ref() = (int)(ref_sizes_[i]);
-          refseq = refseq->next();
-          _ASSERT((char*) refseq - header == scratch_pos_);
+        // *2 because we need an output buf for compress and write
+        // for each input buf
+        buffers_.resize(num_threads_*2);
+        for (int i = 0; i < num_threads_*2; i++) {
+          buffers_[i].reset(new char[buffer_size_]);
+          //LOG(INFO) << "pushing back a buffer";
+          buffer_queue_->push(&buffers_[i]);
         }
-
-        // header is set in buffer, ready to add data
-        zstream.zalloc = Z_NULL;
-        zstream.zfree = Z_NULL;
-
+        
+        compute_status_ = Status::OK();
       }
 
       ~AgdOutputBamOp() override {
-        // if scratch_pos_ not 0, compress and append last bit and close file
+        // drain the queues first
+        //LOG(INFO) << "waiting for c queues ...";
+        while (compress_queue_->size() > 0) {
+          this_thread::sleep_for(chrono::milliseconds(10));
+        }
+       
+        // now stop the threads
+        run_compress_ = false;
+        compress_queue_->unblock();
+        //LOG(INFO) << "Stopping c threads ...";
+        while (num_active_threads_.load() > 1) {
+          this_thread::sleep_for(chrono::milliseconds(10));
+        }
+
+        // it may be that the compress threads give one last 
+        // block to write, we dont want to lose it, so stop the writer
+        // after
+        while (write_queue_->size() > 0) {
+          this_thread::sleep_for(chrono::milliseconds(10));
+        }
+        run_write_ = false;
+        write_queue_->unblock();
+        while (num_active_threads_.load() > 0) {
+          this_thread::sleep_for(chrono::milliseconds(10));
+        }
+
+        // if we have a partial buffer, compress and write it out
+
         if (scratch_pos_ != 0) {
-          CompressAndWrite();
+          BufferRef buf;
+          buffer_queue_->pop(buf);
+          size_t compressed_size = 0;
+          Status s = CompressToBuffer(scratch_, scratch_pos_, buf->get(), buffer_size_, compressed_size);
+          if (!s.ok() || compressed_size == 0) {
+            LOG(ERROR) << "Error in final compress and write, compressed size = " << compressed_size;
+          }
+          int status = fwrite(buf->get(), compressed_size, 1, bam_fp_);
+
+          if (status < 0) 
+            LOG(INFO) << "WARNING: Final write of BAM failed with " << status;
         }
         // EOF marker for bam BGZF format
         static _uint8 eof[] = {
@@ -137,9 +172,43 @@ namespace tensorflow {
         if (status == EOF) 
           LOG(INFO) << "WARNING: Failed to close BAM file pointer: " << status;
 
+        LOG(INFO) << "compress queue push wait: " << compress_queue_->num_push_waits();
+        LOG(INFO) << "write queue push wait: " << write_queue_->num_push_waits();
+        LOG(INFO) << "compress queue pop wait: " << compress_queue_->num_pop_waits();
+        LOG(INFO) << "write queue pop wait: " << write_queue_->num_pop_waits();
+        LOG(INFO) << "buffer queue push wait: " << buffer_queue_->num_push_waits();
+        LOG(INFO) << "buffer queue pop wait: " << buffer_queue_->num_pop_waits();
       }
 
       void Compute(OpKernelContext* ctx) override {
+        if (first_) {
+          Init(ctx);
+          //LOG(INFO) << "getting buffer";
+          buffer_queue_->pop(current_buf_ref_);
+          scratch_ = current_buf_ref_->get();
+          scratch_pos_ = 0;
+          // and write the header into the first buffer
+          BAMHeader* bamHeader = (BAMHeader*) scratch_;
+          bamHeader->magic = BAMHeader::BAM_MAGIC;
+          size_t samHeaderSize = header_.length();
+          memcpy(bamHeader->text(), header_.c_str(), samHeaderSize);
+          bamHeader->l_text = (int)samHeaderSize;
+          scratch_pos_ = BAMHeader::size((int)samHeaderSize);
+
+          bamHeader->n_ref() = ref_seqs_.size();
+          BAMHeaderRefSeq* refseq = bamHeader->firstRefSeq();
+          for (int i = 0; i < ref_seqs_.size(); i++) {
+            int len = ref_seqs_[i].length() + 1;
+            scratch_pos_ += BAMHeaderRefSeq::size(len);
+            refseq->l_name = len;
+            memcpy(refseq->name(), ref_seqs_[i].c_str(), len);
+            refseq->l_ref() = (int)(ref_sizes_[i]);
+            refseq = refseq->next();
+            _ASSERT((char*) refseq - header == scratch_pos_);
+          }
+          first_ = false;
+        }
+
 
         const Tensor *results_in, *bases_in, *qualities_in, *metadata_in, *num_records_t;
         OP_REQUIRES_OK(ctx, ctx->input("num_records", &num_records_t));
@@ -184,21 +253,31 @@ namespace tensorflow {
           OP_REQUIRES_OK(ctx, ParseCigar(cigar, cigar_len, cigar_vec));
           
           size_t bamSize = BAMAlignment::size((unsigned)meta_len + 1, cigar_vec.size(), base_len, /*auxLen*/0);
-          if ((scratch_size_ - scratch_pos_) < bamSize) {
-            // compress and flush
-            OP_REQUIRES_OK(ctx, CompressAndWrite());
+          if ((buffer_size_ - scratch_pos_) < bamSize) {
+            // full buffer, push to compress queue and get a new buffer
+            //LOG(INFO) << "main is getting buf for compress";
+            BufferRef compress_ref;
+            buffer_queue_->pop(compress_ref);
+            //LOG(INFO) << "main is pushing to compress";
+            compress_queue_->push(make_tuple(current_buf_ref_, scratch_pos_, compress_ref, buffer_size_, current_index_));
+
+            //LOG(INFO) << "main is getting fresh buf";
+            current_index_++;
+            buffer_queue_->pop(current_buf_ref_);
+            scratch_ = current_buf_ref_->get();
+            scratch_pos_ = 0;
           }
           
-          BAMAlignment* bam = (BAMAlignment*) (scratch_.get() + scratch_pos_);
+          BAMAlignment* bam = (BAMAlignment*) (scratch_ + scratch_pos_);
           bam->block_size = (int)bamSize - 4;
 
-          int pos = FindChromosome(result->location_, mate_ref_index);
+          int pos = FindChromosome(result->location_, ref_index);
           bam->refID = ref_index;
           bam->pos = pos;
           bam->l_read_name = (_uint8)meta_len + 1;
           bam->MAPQ = result->mapq_;
           
-          int mate_pos = FindChromosome(result->next_location_, ref_index);
+          int mate_pos = FindChromosome(result->next_location_, mate_ref_index);
 
           int refLength = cigar_vec.size() > 0 ? 0 : base_len;
           for (int i = 0; i < cigar_vec.size(); i++) {
@@ -245,8 +324,9 @@ namespace tensorflow {
 
     private:
 
-      Status CompressAndWrite() {
-        if (scratch_pos_ == 0)
+      Status CompressToBuffer(char* in_buf, uint32_t in_size, char* out_buf, uint32_t out_size,
+          size_t &compressed_size) {
+        if (in_size == 0)
           return Internal("attempting to compress and write 0 bytes");
         // set up BAM header structure
         gz_header header;
@@ -271,12 +351,15 @@ namespace tensorflow {
         bamExtraData[4] = 3; // will be filled in later
         bamExtraData[5] = 7; // will be filled in later
 
+        z_stream zstream;
+        zstream.zalloc = Z_NULL;
+        zstream.zfree = Z_NULL;
         const int windowBits = 15;
         const int GZIP_ENCODING = 16;
-        zstream.next_in = (Bytef*) scratch_.get();
-        zstream.avail_in = (uInt)scratch_pos_;
-        zstream.next_out = (Bytef*) scratch_compress_.get();
-        zstream.avail_out = (uInt)scratch_size_;
+        zstream.next_in = (Bytef*) in_buf;
+        zstream.avail_in = (uInt)in_size;
+        zstream.next_out = (Bytef*) out_buf;
+        zstream.avail_out = (uInt)out_size;
         uInt oldAvail;
         int status;
 
@@ -306,20 +389,23 @@ namespace tensorflow {
           return Internal("libz: deflateEnd failed with ", status);
         }
 
-        size_t toUsed = scratch_size_ - zstream.avail_out;
+        size_t toUsed = out_size - zstream.avail_out;
         // backpatch compressed block size into gzip header
         if (toUsed >= BAM_BLOCK) {
           return Internal("exceeded BAM chunk size");
         }
-        * (_uint16*) (scratch_compress_.get() + 16) = (_uint16) (toUsed - 1);
+        * (_uint16*) (out_buf + 16) = (_uint16) (toUsed - 1);
 
-        status = fwrite(scratch_compress_.get(), toUsed, 1, bam_fp_);
+        //status = fwrite(scratch_compress_.get(), toUsed, 1, bam_fp_);
 
-        if (status < 0)
-          return Internal("write compressed block to bam failed: ", status);
+        //if (status < 0)
+          //return Internal("write compressed block to bam failed: ", status);
 
-        scratch_pos_ = 0;
+        if (!((BgzfHeader*)(out_buf))->validate(toUsed, in_size)) {
+          return Internal("bgzf validation failed");
+        }
 
+        compressed_size = toUsed;
         return Status::OK();
       }
 
@@ -345,8 +431,8 @@ namespace tensorflow {
         while(cigar_len > 0) {
           size_t len = parseNextOp(cigar, op, op_len);
           cigar += len;
-          cigar_len -= len;         
-          uint32_t val = (len << 4) | BAMAlignment::CigarToCode[op];
+          cigar_len -= len;
+          uint32_t val = (op_len << 4) | BAMAlignment::CigarToCode[op];
           cigar_vec.push_back(val);
         }
         return Status::OK();
@@ -368,17 +454,154 @@ namespace tensorflow {
         return Status::OK();
       }
 
+      void Init(OpKernelContext* ctx) {
+
+        auto compress_func = [this] () {
+          
+          int my_id = 0;
+          {
+            mutex_lock l(mu_);
+            my_id = thread_id_++;
+          }
+        
+          size_t compressed_size;
+          CompressItem item;
+          while (run_compress_) {
+            
+            if (!compress_queue_->pop(item)) {
+              continue;
+            }
+
+            BufferRef in_buf = get<0>(item);
+            auto in_size = get<1>(item); 
+            BufferRef out_buf = get<2>(item);
+            auto out_size = get<3>(item); 
+            auto index = get<4>(item); 
+
+            compressed_size = 0;
+            //LOG(INFO) << my_id <<  " compressor compressing index " << index << " at size " << in_size << " bytes to " 
+              //<< " output buf with size " << out_size;
+            Status s = CompressToBuffer(in_buf->get(), in_size, out_buf->get(), out_size, compressed_size);
+            if (!s.ok()) {
+              LOG(ERROR) << "Error in compress and write";
+              compute_status_ = s;
+              return;
+            }
+            //LOG(INFO) << my_id << " compressed into " << compressed_size << " bytes.";
+
+            buffer_queue_->push(in_buf); // recycle buffer
+            WriteItem wr_item;
+            wr_item.buf = out_buf;
+            wr_item.size = compressed_size;
+            wr_item.index = index;
+            //LOG(INFO)<< my_id  << " compressor pushing " << index << " to writer ";
+            write_queue_->push(wr_item);
+          }
+          num_active_threads_--;
+        };
+
+        auto writer_func = [this] () {
+          int my_id = 0;
+          {
+            mutex_lock l(mu_);
+            my_id = thread_id_++;
+          }
+     
+          uint32_t index = 0;
+          WriteItem item;
+          while (run_write_) {
+            
+            if (!write_queue_->peek(item)) {
+              continue;
+            }
+            // its possible the next index is not in the queue yet
+            // wait for it
+            if (item.index != index)
+              continue;
+
+            // got the right index now, works because
+            // we know a lower index than `index` will never
+            // happen
+            //LOG(INFO) << my_id << " writer popping";
+            write_queue_->pop(item);
+
+            auto* buf = item.buf;
+            auto size = item.size;
+            auto idx = item.index;
+
+            //LOG(INFO) << my_id << " writer writing index " << idx;
+            if (idx != index) {
+              LOG(INFO) << my_id << " got " << idx << " for index " << index;
+              compute_status_ = Internal("Did not get block index in order!");
+              return;
+            }
+
+            int status = fwrite(buf->get(), size, 1, bam_fp_);
+            if (status < 0) {
+              compute_status_ = Internal("Failed to write to bam file");
+              return;
+            }
+            index++;
+
+            // recycle the buffer
+            //LOG(INFO) << my_id << "writer recycling";
+            buffer_queue_->push(buf);
+          }
+          num_active_threads_--;
+        };
+
+        auto worker_threadpool = ctx->device()->tensorflow_cpu_worker_threads()->workers;
+        for (int i = 0; i < num_threads_-1; i++)
+          worker_threadpool->Schedule(compress_func);
+        worker_threadpool->Schedule(writer_func);
+
+        num_active_threads_ = num_threads_;
+      }
+
       vector<string> ref_seqs_;
       vector<int32> ref_sizes_;
       vector<int64> ref_size_totals_;
-      unique_ptr<char> scratch_, scratch_compress_;
-      uint64_t scratch_pos_ = 0;
-      static const uint64_t scratch_size_ = 64*1024; // 64Kb
+      string header_;
+  
+      mutex mu_;
+      bool first_ = true;
+      uint32_t current_index_ = 0;
+      //unique_ptr<char> scratch_, scratch_compress_;
+      //uint64_t scratch_pos_ = 0;
+      const uint64_t buffer_size_ = 64*1024; // 64Kb
       FILE* bam_fp_ = nullptr;
-    
-      z_stream zstream;
       
+      volatile bool run_compress_ = true;
+      volatile bool run_write_ = true;
+      atomic<uint32_t> num_active_threads_;
+      int thread_id_ = 0;
+      int num_threads_;
 
+      typedef const unique_ptr<char[]>* BufferRef;
+      unique_ptr<WorkQueue<BufferRef>> buffer_queue_;
+
+      typedef tuple<BufferRef, uint32_t, BufferRef, uint32_t, uint32_t> CompressItem; // inbuffer, isize, outbuffer, osize, index (ordering)
+      unique_ptr<WorkQueue<CompressItem>> compress_queue_;
+
+      struct WriteItem {
+        BufferRef buf;
+        size_t size;
+        uint32_t index;
+        bool operator<(const WriteItem& other) const {
+          return index > other.index;
+        }
+      };
+
+      unique_ptr<PriorityWorkQueue<WriteItem>> write_queue_;
+
+
+      vector<unique_ptr<char[]>> buffers_;
+
+      BufferRef current_buf_ref_;
+      char* scratch_;
+      uint32_t scratch_pos_;
+
+      Status compute_status_;
       TF_DISALLOW_COPY_AND_ASSIGN(AgdOutputBamOp);
   };
 
---
+++ tensorflow/core/user_ops/bam/work_queue.h
@@ -0,0 +1,202 @@
+#pragma once
+
+#include <queue>
+#include <utility>
+#include "tensorflow/core/platform/mutex.h"
+#include "tensorflow/core/platform/logging.h"
+
+namespace tensorflow {
+
+// a class wrapping STL queue 
+// thread-safe, limited buffer capacity, blocks on push()
+// to a full queue. 
+
+template <typename T>
+class WorkQueue {
+  public:
+
+    WorkQueue(int capacity);
+    ~WorkQueue() { LOG(INFO) << "num pushed: " << num_push_; } // if you don't define the destructor, you get
+                     // a weird linker error
+
+    // return true if pushed, false otherwise
+    // will block until pushed if block_ is true
+    bool push(const T& item);
+    // return true if success and item is valid, false otherwise
+    bool pop(T& item);
+
+    void drop_if_equal(T& item);
+
+    bool peek(T& item);
+
+    // unblock the queue, notify all threads
+    void unblock();
+    // set blocking behavior
+    void set_block();
+
+    bool empty() const;
+    size_t capacity() const;
+    size_t size() const;
+
+    int64 num_pop_waits();
+    int64 num_push_waits();
+    int64 num_peek_waits();
+
+  private:
+    // mutex to protect the queue
+    mutable mutex mu_;
+    // cond vars for block/wait/notify on queue push/pop
+    mutable std::condition_variable queue_pop_cv_;
+    mutable std::condition_variable queue_push_cv_;
+    std::queue<T> queue_;
+    size_t capacity_;
+    // block on calls to push, pop
+    bool block_ = true;
+    int64 num_pop_waits_ = 0;
+    int64 num_push_waits_ = 0;
+    int64 num_peek_waits_ = 0;
+    int64 num_push_ = 0;
+
+ };
+
+template <typename T>
+bool WorkQueue<T>::peek(T& item) {
+  bool popped = false;
+  {
+    mutex_lock l(mu_);
+
+    if (queue_.empty() && block_) {
+      num_peek_waits_++;
+      queue_pop_cv_.wait(l, [this]() {
+          return !queue_.empty() || !block_;
+        });
+    }
+
+    if (!queue_.empty()) {
+      item = queue_.front();
+      popped = true;
+    }
+  }
+  if (popped)
+    queue_pop_cv_.notify_one();
+  return popped;
+}
+
+template <typename T>
+void WorkQueue<T>::drop_if_equal(T& item) {
+  {
+    mutex_lock l(mu_);
+    if (!queue_.empty() && queue_.front() == item) {
+      queue_.pop();
+    }
+  }
+  queue_push_cv_.notify_one();
+}
+
+template <typename T>
+bool WorkQueue<T>::pop(T& item) {
+
+  bool popped = false;
+  {
+    mutex_lock l(mu_);
+    if (queue_.empty() && block_) {
+      num_pop_waits_++;
+      queue_pop_cv_.wait(l, [this]() {
+          return !queue_.empty() || !block_;
+          });
+    }
+
+    if (!queue_.empty()) {
+      item = queue_.front();
+      queue_.pop();
+      popped = true;
+    }
+  }
+  if (popped) {
+    // tell someone blocking on write they can now write to the queue
+    queue_push_cv_.notify_one();
+    return true;
+  } else
+    return false;
+}
+
+template <typename T>
+bool WorkQueue<T>::push(const T& item) {
+
+  bool pushed = false;
+  {
+    mutex_lock l(mu_);
+    // we block until something pops and makes room for us
+    // unless blocking is set to false
+    if (queue_.size() == capacity_ && block_) {
+      num_push_waits_++;
+      queue_push_cv_.wait(l, [this]() {
+          return (queue_.size() < capacity_) || !block_;
+        });
+    }
+
+    if (queue_.size() < capacity_) {
+      queue_.push(item);
+      pushed = true;
+    }
+  }
+
+  if (pushed) {
+    // tell someone blocking on read they can now read from the queue
+    // TODO maybe notify_all is better here? If so, good to drop the notify_one in peek
+    queue_pop_cv_.notify_one();
+    num_push_++;
+    return true;
+  } else
+    return false;
+
+}
+
+template <typename T>
+void WorkQueue<T>::unblock() {
+  {
+    mutex_lock l(mu_);
+    VLOG(INFO) << "WorkQueue("<< this << ") unblock called!";
+    block_ = false;
+  }
+
+  queue_push_cv_.notify_all();
+  queue_pop_cv_.notify_all();
+}
+
+template <typename T>
+void WorkQueue<T>::set_block() {
+  mutex_lock l(mu_);
+  block_ = true;
+}
+
+template <typename T>
+WorkQueue<T>::WorkQueue(int capacity) {
+  // root cause of this joke is that tensorflow attributes 
+  // do not have an unsigned type. `\_(o_o)_/`
+  if (capacity < 0)
+    capacity_ = 10; // a sane default
+  else
+    capacity_ = (size_t) capacity;
+}
+
+template <typename T>
+bool WorkQueue<T>::empty() const { return queue_.empty(); }
+
+template <typename T>
+size_t WorkQueue<T>::capacity() const { return capacity_; }
+
+template <typename T>
+size_t WorkQueue<T>::size() const { return queue_.size(); }
+
+template <typename T>
+int64 WorkQueue<T>::num_pop_waits() { return num_pop_waits_; }
+
+template <typename T>
+int64 WorkQueue<T>::num_push_waits() { return num_push_waits_; }
+
+template <typename T>
+int64 WorkQueue<T>::num_peek_waits() { return num_peek_waits_; }
+
+}  // namespace tensorflow
+
