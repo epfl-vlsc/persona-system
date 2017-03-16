7258911f3ab9e8f04560b09559f624a6ab588d30
Stuart Byma <stuart.byma@epfl.ch>
2017-03-16 11:21:14 +0100

    fixing contrib build to include persona, fixing sync call in file map

--- tensorflow/contrib/persona/kernels/agd-format/file_mmap_op.cc
+++ tensorflow/contrib/persona/kernels/agd-format/file_mmap_op.cc
@@ -57,7 +57,7 @@ namespace tensorflow {
       OP_REQUIRES_OK(ctx, ref_pool->GetResource(&mmf));
 
       unique_ptr<ReadOnlyMemoryRegion> rmr;
-      OP_REQUIRES_OK(ctx, ctx->env()->NewReadOnlyMemoryRegionFromFile(filename, &rmr, synchronous_));
+      OP_REQUIRES_OK(ctx, ctx->env()->NewReadOnlyMemoryRegionFromFile(filename, &rmr/*, synchronous_*/));
       mmf->get()->own(move(rmr));
 
       Tensor *file_handles, *file_names;
