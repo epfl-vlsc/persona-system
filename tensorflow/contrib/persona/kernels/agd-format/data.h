8d52e844b81283e65a7ea5796561d44a0b5d4a74
Sam Whitlock <sam.whitlock@epfl.ch>
2017-04-28 10:49:32 +0200

    moving 2 bit converter op out

--- tensorflow/contrib/persona/kernels/agd-format/data.h
+++ tensorflow/contrib/persona/kernels/agd-format/data.h
@@ -6,8 +6,9 @@ namespace tensorflow {
   class Data {
   public:
     virtual const char* data() const = 0;
+    virtual char* mutable_data();
     virtual std::size_t size() const = 0;
-    virtual void release() {};
+    virtual void release();
   };
 
   class DataReleaser {
