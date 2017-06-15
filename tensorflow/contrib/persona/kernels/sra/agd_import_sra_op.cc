3b56874317d1cc10a97d83d0c45252ce4863fd36
Alaleh Azhir <azhir@iccluster129.iccluster.epfl.ch>
2017-06-15 15:44:42 +0200

    Added comments

--- tensorflow/contrib/persona/kernels/sra/agd_import_sra_op.cc
+++ tensorflow/contrib/persona/kernels/sra/agd_import_sra_op.cc
@@ -13,14 +13,9 @@
 #include <vector>
 #include "tensorflow/contrib/persona/kernels/object-pool/resource_container.h"
 #include "tensorflow/contrib/persona/kernels/object-pool/ref_pool.h"
-//#include "tensorflow/contrib/persona/kernels/agd-format/agd_result_reader.h"
 #include "tensorflow/contrib/persona/kernels/agd-format/column_builder.h"
 #include "tensorflow/contrib/persona/kernels/agd-format/format.h"
 #include "tensorflow/contrib/persona/kernels/agd-format/sam_flags.h"
-//#include "tensorflow/contrib/persona/kernels/agd-format/proto/alignment.pb.h"
-//#include "tensorflow/contrib/persona/kernels/snap-align/snap/SNAPLib/Bam.h"
-//#include "tensorflow/contrib/persona/kernels/snap-align/snap/SNAPLib/Read.h"
-//#include "tensorflow/contrib/persona/kernels/snap-align/snap/SNAPLib/ReadSupplierQueue.h"
 #include "zlib.h"
 
 // Required to read the SRA file
@@ -29,7 +24,6 @@
 #include <ngs/ReadCollection.hpp>
 #include <ngs/ReadIterator.hpp>
 #include <iostream>
-#include <string>
 
 namespace tensorflow {
   using namespace std;
@@ -38,16 +32,8 @@ namespace tensorflow {
   using namespace ngs;
 
   using shape_inference::InferenceContext;
-   
-/*  namespace { 
-    void resource_releaser(ResourceContainer<Data> *data) {
-      core::ScopedUnref a(data);
-      data->release();
-    }
-  }*/
 
   class AgdImportSraOp : public OpKernel {
-    //ReadIterator iterator;
     unsigned int numReads;
     public:
       // This Op WILL throw OutOfRange upon Sra file read completion, 
@@ -59,36 +45,11 @@ namespace tensorflow {
         OP_REQUIRES_OK(ctx, ctx->GetAttr("path", &path));
         OP_REQUIRES_OK(ctx, ctx->GetAttr("num_threads", &num_threads));
         OP_REQUIRES_OK(ctx, ctx->GetAttr("chunk_size", &chunk_size_));
-        /*OP_REQUIRES_OK(ctx, ctx->GetAttr("unaligned", &unaligned_));
-        OP_REQUIRES_OK(ctx, ctx->GetAttr("ref_seq_lens", &ref_seq_lens_));
-
-        // build a dummy genome so SNAP BAMreader will give us correct
-        // location information from an alignment
-        genome_.reset(new Genome(0xffffffff, 0xffffffff, 0));
-
-        ref_size_totals_.reserve(ref_seq_lens_.size());
-        int64 total = 0;
-        vector<char> dummy;
-        for (auto len : ref_seq_lens_) {
-          genome_->startContig("test");
-          dummy.resize(len);
-          genome_->addData(&dummy[0], len);
-
-          total += len;
-          //ref_vec.push_back(RefData(ref_seqs_[i], ref_sizes_[i]));
-          ref_size_totals_.push_back(total);
-        }
-        genome_->fillInContigLengths();
-
-        reader_context_.genome = genome_.get();
-        reader_ = new BAMReader(reader_context_);
-        reader_->init(path.c_str(), ReadSupplierQueue::BufferCount(num_threads), 0, 0);
-*/
-	// initialize the Sra Reader
-	cout << "path is " << path.c_str() << "\n"; 
+	
+        // initialize the Sra Reader
 	ReadCollection reader = ncbi::NGS::openReadCollection ( path.c_str() );
 	numReads = reader.getReadCount();
-	cout << "reads" << numReads << "\n";
+	cout << "Number of reads: " << numReads << "\n";
 	iterator = new ReadIterator(reader.getReadRange(1, numReads));
       }
     
@@ -109,7 +70,6 @@ namespace tensorflow {
       ~AgdImportSraOp() override {
         // drain the queues first
         core::ScopedUnref unref_listpool(bufpair_pool_);
-        //delete reader_;
 	delete iterator;
       }
 
@@ -130,12 +90,8 @@ namespace tensorflow {
         }
 
         Tensor* out_t, *num_recs_t, *first_ord_t;
-     //   if (unaligned_) {
-          OP_REQUIRES_OK(ctx, ctx->allocate_output("chunk_out", TensorShape({3, 2}), &out_t));
-       // } else {
-         // OP_REQUIRES_OK(ctx, ctx->allocate_output("chunk_out", TensorShape({4, 2}), &out_t));
-       // }
-
+        
+        OP_REQUIRES_OK(ctx, ctx->allocate_output("chunk_out", TensorShape({3, 2}), &out_t));
       	OP_REQUIRES_OK(ctx, ctx->allocate_output("num_records", TensorShape({}), &num_recs_t));
         OP_REQUIRES_OK(ctx, ctx->allocate_output("first_ordinal", TensorShape({}), &first_ord_t));
 
@@ -151,15 +107,8 @@ namespace tensorflow {
         OP_REQUIRES_OK(ctx, GetBufferForBuilder(ctx, meta_builder, out_t, 2));
 
         Status s;
-        // if paired, num_recs_out will be 2x chunksize
         int num_recs_out;
-        //if (unaligned_) {
-          s = Process(base_builder, qual_builder, meta_builder, num_recs_out);
-       // } else {
-         // AlignmentResultBuilder results_builder;
-         // OP_REQUIRES_OK(ctx, GetBufferForBuilder(ctx, results_builder, out_t, 3));
-         // s = ProcessAligned(base_builder, qual_builder, meta_builder, results_builder, num_recs_out);
-        //}
+        s = Process(base_builder, qual_builder, meta_builder, num_recs_out);
         num_recs = num_recs_out;
         first_ordinal_ += num_recs_out;
         OP_REQUIRES_OK(ctx, s);
@@ -187,156 +136,15 @@ namespace tensorflow {
 	return Status::OK();
       }
 
-      Status ProcessUnaligned(ColumnBuilder& bases, ColumnBuilder& qual, ColumnBuilder& meta, int& num_recs) {
-      /*  Read bam_read;
-        AlignmentResult result;
-        GenomeLocation location;
-        bool isRC;
-        unsigned mapq;
-        unsigned flag;
-        const char* cigar;
-        num_recs = 0;
-        for (size_t i = 0; i < chunk_size_; i++) {
-          if (!reader_->getNextRead(&bam_read, &result, &location, &isRC, &mapq, &flag, 
-                &cigar)) {
-            return OutOfRange("No more reads in BAM file");
-          }
-
-          TF_RETURN_IF_ERROR(IntoBases(bam_read.getUnclippedData(), bam_read.getUnclippedLength(), bases_));
-
-          bases.AppendRecord(reinterpret_cast<const char*>(&bases_[0]), sizeof(BinaryBases)*bases_.size());
-          qual.AppendRecord(bam_read.getUnclippedQuality(), bam_read.getUnclippedLength());
-          meta.AppendRecord(bam_read.getId(), bam_read.getIdLength());
-          num_recs++;
-        }*/
-        return Status::OK();
-      }
-      
-      /*Status ProcessAligned(ColumnBuilder& bases, ColumnBuilder& qual, ColumnBuilder& meta,
-          AlignmentResultBuilder& results, int& num_recs) {
-        Read bam_read, mate_bam_read;
-        AlignmentResult result;
-        GenomeLocation location, mate_location;
-        bool isRC, mate_isRC;
-        unsigned mapq;
-        unsigned flag;
-        const char* cigar;
-        Alignment alignment, mate_alignment;
-        int64 position;
-        int32 ref_idx;
-        int64 mate_position;
-        int32 mate_ref_idx;
-        num_recs = 0;
-        for (size_t i = 0; i < chunk_size_; i++) {
-          if (!reader_->getNextRead(&bam_read, &result, &location, &isRC, &mapq, &flag, 
-                &cigar)) {
-            return OutOfRange("No more reads in BAM file");
-          }
-          if (IsSecondary(flag) || IsSupplemental(flag)) {// skip secondary/supplemental
-            i--;
-            continue;
-          }
-          if (isRC) {
-            bam_read.becomeRC();
-          }
-          TF_RETURN_IF_ERROR(IntoBases(bam_read.getUnclippedData(), bam_read.getUnclippedLength(), bases_));
-
-          bases.AppendRecord(reinterpret_cast<const char*>(&bases_[0]), sizeof(BinaryBases)*bases_.size());
-          qual.AppendRecord(bam_read.getUnclippedQuality(), bam_read.getUnclippedLength());
-          meta.AppendRecord(bam_read.getId(), bam_read.getIdLength());
-          position = FindChromosome(location, ref_idx);
-          alignment.mutable_position()->set_position(position);
-          alignment.mutable_position()->set_ref_index(ref_idx);
-          alignment.set_mapping_quality(mapq);
-          alignment.set_flag(flag);
-          alignment.set_cigar(cigar);
-          alignment.set_template_length(0);
-          num_recs++;
-          
-          if (IsPaired(flag)) {
-            if (!reader_->getNextRead(&mate_bam_read, &result, &mate_location, &mate_isRC, &mapq, &flag, 
-                  &cigar)) {
-              return Internal("pair not found for read!!");
-            }
-            if (mate_isRC) {
-              mate_bam_read.becomeRC();
-            }
-            TF_RETURN_IF_ERROR(IntoBases(mate_bam_read.getData(), mate_bam_read.getDataLength(), bases_));
-
-            bases.AppendRecord(reinterpret_cast<const char*>(&bases_[0]), sizeof(BinaryBases)*bases_.size());
-            qual.AppendRecord(mate_bam_read.getUnclippedQuality(), mate_bam_read.getUnclippedLength());
-            meta.AppendRecord(mate_bam_read.getId(), mate_bam_read.getIdLength());
-            mate_position = FindChromosome(mate_location, mate_ref_idx);
-            mate_alignment.mutable_position()->set_position(mate_position);
-            mate_alignment.mutable_position()->set_ref_index(mate_ref_idx);
-            mate_alignment.set_mapping_quality(mapq);
-            mate_alignment.set_flag(flag);
-            mate_alignment.set_cigar(cigar);
-           
-            // deal with paired alignment stuff, some of this copied from SNAP
-            alignment.mutable_next_position()->set_position(mate_position);
-            alignment.mutable_next_position()->set_ref_index(mate_ref_idx);
-            mate_alignment.mutable_next_position()->set_position(position);
-            mate_alignment.mutable_next_position()->set_ref_index(ref_idx);
-            int64 basesClippedBefore, basesClippedAfter;
-            int64 clippedLength = bam_read.getDataLength();
-            int64 fullLength = bam_read.getUnclippedLength();
-            if (isRC) {
-              basesClippedBefore = fullLength - clippedLength - bam_read.getFrontClippedLength();
-              basesClippedAfter = bam_read.getFrontClippedLength();
-            } else {
-              basesClippedBefore = bam_read.getFrontClippedLength();
-              basesClippedAfter = fullLength - clippedLength - basesClippedBefore;
-            }
-            GenomeLocation myStart = location - basesClippedBefore;
-            GenomeLocation myEnd = location + clippedLength + basesClippedAfter;
-            int64 mateBasesClippedBefore = mate_bam_read.getFrontClippedLength();
-            int64 mateBasesClippedAfter = mate_bam_read.getUnclippedLength() - mate_bam_read.getDataLength() - mateBasesClippedBefore;
-            GenomeLocation mateStart = mate_location - (mate_isRC == RC ? mateBasesClippedAfter : mateBasesClippedBefore);
-            GenomeLocation mateEnd = mate_location + mate_bam_read.getDataLength() + (!mate_isRC ? mateBasesClippedAfter : mateBasesClippedBefore);
-            int template_length;
-            if (ref_idx == mate_ref_idx) { 
-              if (myStart < mateStart) {
-                template_length = mateEnd - myStart;
-              } else {
-                template_length = -(myEnd - mateStart);
-              }
-            } // otherwise leave TLEN as zero.
-            alignment.set_template_length(template_length);
-            mate_alignment.set_template_length(-template_length);
-            results.AppendAlignmentResult(alignment);
-            results.AppendAlignmentResult(mate_alignment);
-
-            num_recs++;
-          } else
-            results.AppendAlignmentResult(alignment);
-
-        }
-        return Status::OK();
-      }*/
-
       ReferencePool<BufferPair> *bufpair_pool_ = nullptr;
 
-     /* int64 FindChromosome(int64 location, int &ref_idx) {
-        int index = 0;
-        while (location > ref_size_totals_[index]) index++;
-        ref_idx = index;
-        return (index == 0) ? (int)location : (int)(location - ref_size_totals_[index-1]);
-      }*/
-
-     // unique_ptr<Genome> genome_;
-     // bool unaligned_;
-  //    BAMReader* reader_ = nullptr;
-    //  ReaderContext reader_context_; 
       int chunk_size_;
       ReadIterator* iterator = nullptr;
       int64 first_ordinal_ = 0;
-    //  vector<int> ref_seq_lens_;
-     // vector<int64> ref_size_totals_;;
       TF_DISALLOW_COPY_AND_ASSIGN(AgdImportSraOp);
   };
 
 
   REGISTER_KERNEL_BUILDER(Name("AgdImportSra").Device(DEVICE_CPU), AgdImportSraOp);
 
-}  // namespace tensorflow
+}  
