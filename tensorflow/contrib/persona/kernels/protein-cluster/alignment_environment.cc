b370beadf516205bddd9712ee3bb93aef6f0c462
Stuart Byma <stuart.byma@epfl.ch>
2018-03-05 16:00:48 +0100

    forgot to save some changes

---
+++ tensorflow/contrib/persona/kernels/protein-cluster/alignment_environment.cc
@@ -0,0 +1,16 @@
+
+#include "tensorflow/contrib/persona/kernels/protein_clustering/alignment_environment.h"
+#include "tensorflow/core/platform/file_sstem.h"
+
+namespace tensorflow {
+
+  void AlignmentEnvironments::EstimatePam(char* seq1, char* seq2, int len) {
+
+  }
+  
+  const AlignmentEnvironment& AlignmentEnvironments::FindNearest(double pam) {
+
+  }
+
+
+}
